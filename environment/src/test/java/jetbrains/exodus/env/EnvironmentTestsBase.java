f5271cf238a94d8c542fa3725f7a9c437c8cf028
Vyacheslav Lukianov <lvo@intellij.net>
2014-10-12 15:10:36 +0400

    fixing TransactionTest

--- environment/src/test/java/jetbrains/exodus/env/EnvironmentTestsBase.java
+++ environment/src/test/java/jetbrains/exodus/env/EnvironmentTestsBase.java
@@ -270,7 +270,7 @@ public class EnvironmentTestsBase {
     protected void assertNotNullStringValue(final Store store,
                                             final ByteIterable keyEntry,
                                             final String value) {
-        env.executeInReadonlyTransaction(new TransactionalExecutable() {
+        env.executeInTransaction(new TransactionalExecutable() {
             @Override
             public void execute(@NotNull Transaction txn) {
                 assertNotNullStringValue(txn, store, keyEntry, value);
