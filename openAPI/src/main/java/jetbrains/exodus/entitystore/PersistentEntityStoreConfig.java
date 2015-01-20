d9a31735c9234bccf0c53beef52defd9f260843d
Pavel.Nikitin <pavel.nikitin@jetbrains.com>
2015-01-20 17:25:24 +0400

    fix NPE

--- openAPI/src/main/java/jetbrains/exodus/entitystore/PersistentEntityStoreConfig.java
+++ openAPI/src/main/java/jetbrains/exodus/entitystore/PersistentEntityStoreConfig.java
@@ -75,6 +75,7 @@ public final class PersistentEntityStoreConfig extends AbstractConfig {
                 new Pair(REFACTORING_DELETE_REDUNDANT_BLOBS, false),
                 new Pair(MAX_IN_PLACE_BLOB_SIZE, 10000),
                 new Pair(CACHING_DISABLED, false),
+                new Pair(REORDERING_DISABLED, false),
                 new Pair(EXPLAIN_ON, false),
                 new Pair(UNIQUE_INDICES_USE_BTREE, false),
                 new Pair(DEBUG_LINK_DATA_GETTER, false),
@@ -148,6 +149,10 @@ public final class PersistentEntityStoreConfig extends AbstractConfig {
 
     public void setCachingDisabled(final boolean disabled) {
         setSetting(CACHING_DISABLED, disabled);
+    }
+
+    public void setReorderingDisabled(final boolean disabled) {
+        setSetting(REORDERING_DISABLED, disabled);
     }
 
     public boolean isExplainOn() {
