bad3ed84803e94df5c5001538854d4fa0725e9b0
Pavel.Nikitin <pavel.nikitin@jetbrains.com>
2015-03-17 19:04:08 +0400

    remove redundant code

--- entity-store/src/main/java/jetbrains/exodus/entitystore/ReadonlyPersistentStoreTransaction.java
+++ entity-store/src/main/java/jetbrains/exodus/entitystore/ReadonlyPersistentStoreTransaction.java
@@ -15,32 +15,12 @@
  */
 package jetbrains.exodus.entitystore;
 
-import jetbrains.exodus.env.ReadonlyTransactionException;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
 class ReadonlyPersistentStoreTransaction extends PersistentStoreTransaction {
 
     ReadonlyPersistentStoreTransaction(@NotNull final PersistentEntityStoreImpl store) {
         super(store, true);
-    }
-
-    @Override
-    void addBlob(long blobHandle, @NotNull InputStream stream) throws IOException {
-        throw new ReadonlyTransactionException();
-    }
-
-    @Override
-    void addBlob(long blobHandle, @NotNull File file) {
-        throw new ReadonlyTransactionException();
-    }
-
-    @Override
-    void deleteBlob(long blobHandle) {
-        throw new ReadonlyTransactionException();
     }
 
     @Override
