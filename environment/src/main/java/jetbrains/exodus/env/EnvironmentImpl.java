f6047f413a17b0bde30d7cfece4d09d7f18f4a6d
Evgenii Schepotiev <evgenii.schepotiev@jetbrains.com>
2015-06-24 14:53:18 +0200

    review #XD-454 fixed

--- environment/src/main/java/jetbrains/exodus/env/EnvironmentImpl.java
+++ environment/src/main/java/jetbrains/exodus/env/EnvironmentImpl.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010 - 2015 JetBrains s.r.o.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -661,7 +661,7 @@ public class EnvironmentImpl implements Environment {
             Throwable trace = transaction.getTrace();
             if (trace == null) {
                 String stacksUnavailable = "Transactions stack traces are not available, " +
-                        "set \'exodus.env.monitorTxns.timeout > 0\'";
+                        "set \'" + EnvironmentConfig.ENV_MONITOR_TXNS_TIMEOUT + " > 0\'";
                 if (debug) {
                     logger.debug(stacksUnavailable);
                 } else {
