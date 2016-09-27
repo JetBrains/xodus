JetBrains/xodus

commit 0dff56fdb2525803c950b97fd6d2469357075703

completely got rid of ExpirationChecker; GC doesn't try to track fragmented utilization, only number of free bytes per file is tracked; #XD-549 fixed

environment/src/main/java/jetbrains/exodus/tree/patricia/PatriciaTreeMutable.java
@@ -290,15 +290,8 @@ final class PatriciaTreeMutable extends PatriciaTreeBase implements ITreeMutable
     }
 
     @Override
-    public boolean reclaim(@NotNull final RandomAccessLoggable loggable,
-                           @NotNull final Iterator<RandomAccessLoggable> loggables) {
-        return reclaim(loggable, loggables, IExpirationChecker.NONE);
-    }
-
-    @Override
     public boolean reclaim(@NotNull RandomAccessLoggable loggable,
-                           @NotNull final Iterator<RandomAccessLoggable> loggables,
-                           @NotNull final IExpirationChecker expirationChecker) {
+                           @NotNull final Iterator<RandomAccessLoggable> loggables) {
         long minAddress = loggable.getAddress();
         while (true) {
             final byte type = loggable.getType();
@@ -337,7 +330,7 @@ final class PatriciaTreeMutable extends PatriciaTreeBase implements ITreeMutable
         }
 
         final PatriciaReclaimActualTraverser actual = new PatriciaReclaimActualTraverser(this);
-        reclaim(new PatriciaReclaimSourceTraverser(sourceTree, sourceRoot, expirationChecker, minAddress), actual);
+        reclaim(new PatriciaReclaimSourceTraverser(sourceTree, sourceRoot, minAddress), actual);
         return actual.wasReclaim || sourceRoot.getAddress() == root.sourceAddress;
     }
 
@@ -441,16 +434,18 @@ final class PatriciaTreeMutable extends PatriciaTreeBase implements ITreeMutable
 
     private static void reclaim(@NotNull final PatriciaReclaimSourceTraverser source,
                                 @NotNull final PatriciaReclaimActualTraverser actual) {
-        if (actual.currentNode.getAddress() == source.currentNode.getAddress()) {
-            actual.currentNode = actual.currentNode.getMutableCopy(actual.mainTree);
+        final NodeBase actualNode = actual.currentNode;
+        final NodeBase sourceNode = source.currentNode;
+        if (actualNode.getAddress() == sourceNode.getAddress()) {
+            actual.currentNode = actualNode.getMutableCopy(actual.mainTree);
             actual.getItr();
             actual.wasReclaim = true;
             reclaimActualChildren(source, actual);
         } else {
             @NotNull
-            ByteIterator srcItr = source.currentNode.keySequence.iterator();
+            ByteIterator srcItr = sourceNode.keySequence.iterator();
             @NotNull
-            ByteIterator actItr = actual.currentNode.keySequence.iterator();
+            ByteIterator actItr = actualNode.keySequence.iterator();
             int srcPushes = 0;
             int actPushes = 0;
             while (true) {
@@ -472,7 +467,7 @@ final class PatriciaTreeMutable extends PatriciaTreeBase implements ITreeMutable
                         actItr = actual.currentNode.keySequence.iterator();
                     }
                 } else if (actItr.hasNext()) {
-                    final NodeChildrenIterator children = source.currentNode.getChildren(actItr.next());
+                    final NodeChildrenIterator children = sourceNode.getChildren(actItr.next());
                     final ChildReference child = children.getNode();
                     if (child == null || !source.isAddressReclaimable(child.suffixAddress)) {
                         break; // child can be expired if source parent was already not-current
@@ -481,7 +476,7 @@ final class PatriciaTreeMutable extends PatriciaTreeBase implements ITreeMutable
                     source.currentIterator = children;
                     source.moveDown();
                     ++srcPushes;
-                    srcItr = source.currentNode.keySequence.iterator();
+                    srcItr = sourceNode.keySequence.iterator();
                 } else { // both iterators matched, here comes the branching
                     reclaimChildren(source, actual);
                     break;
@@ -515,8 +510,6 @@ final class PatriciaTreeMutable extends PatriciaTreeBase implements ITreeMutable
 
     private static void reclaimChildren(@NotNull final PatriciaReclaimSourceTraverser source,
                                         @NotNull final PatriciaReclaimActualTraverser actual) {
-        final long parentAddress = source.currentNode.getAddress();
-        final IExpirationChecker expirationChecker = source.expirationChecker;
         source.moveToNextReclaimable();
         while (source.isValidPos() && actual.isValidPos()) {
             final ChildReference sourceChild = source.currentChild;
@@ -527,16 +520,11 @@ final class PatriciaTreeMutable extends PatriciaTreeBase implements ITreeMutable
             } else if (sourceByte > actualByte) {
                 actual.moveRight();
             } else {
-                final long suffixAddress = sourceChild.suffixAddress;
                 source.moveDown();
-                final long nextLoggableAddress = suffixAddress + source.getTree().getLoggable(source.currentNode.getAddress()).length(); //TODO: don't re-read
                 actual.moveDown();
                 reclaim(source, actual);
                 actual.popAndMutate();
                 source.moveUp();
-                if (expirationChecker.expired(nextLoggableAddress, (int) (parentAddress - nextLoggableAddress))) {
-                    break;
-                }
                 source.moveRight();
                 actual.moveRight();
             }

environment/src/main/java/jetbrains/exodus/env/ContextualTemporaryEmptyStore.java
@@ -17,7 +17,6 @@ package jetbrains.exodus.env;
 
 import jetbrains.exodus.ByteIterable;
 import jetbrains.exodus.log.RandomAccessLoggable;
-import jetbrains.exodus.tree.IExpirationChecker;
 import jetbrains.exodus.tree.ITreeCursor;
 import jetbrains.exodus.tree.TreeMetaInfo;
 import org.jetbrains.annotations.NotNull;
@@ -84,8 +83,7 @@ class ContextualTemporaryEmptyStore extends ContextualStoreImpl {
     @Override
     public void reclaim(@NotNull final Transaction transaction,
                         @NotNull final RandomAccessLoggable loggable,
-                        @NotNull final Iterator<RandomAccessLoggable> loggables,
-                        @NotNull final IExpirationChecker expirationChecker) {
+                        @NotNull final Iterator<RandomAccessLoggable> loggables) {
         // nothing to reclaim
     }
 

environment/src/main/java/jetbrains/exodus/env/StoreImpl.java
@@ -20,7 +20,6 @@ import jetbrains.exodus.ByteIterable;
 import jetbrains.exodus.log.Log;
 import jetbrains.exodus.log.Loggable;
 import jetbrains.exodus.log.RandomAccessLoggable;
-import jetbrains.exodus.tree.IExpirationChecker;
 import jetbrains.exodus.tree.ITree;
 import jetbrains.exodus.tree.TreeMetaInfo;
 import jetbrains.exodus.tree.btree.BTree;
@@ -156,16 +155,15 @@ public class StoreImpl implements Store {
 
     public void reclaim(@NotNull final Transaction transaction,
                         @NotNull final RandomAccessLoggable loggable,
-                        @NotNull final Iterator<RandomAccessLoggable> loggables,
-                        @NotNull final IExpirationChecker expirationChecker) {
+                        @NotNull final Iterator<RandomAccessLoggable> loggables) {
         final TransactionImpl txn = EnvironmentImpl.throwIfReadonly(transaction, "Can't reclaim in read-only transaction");
         final boolean wasTreeCreated = txn.hasTreeMutable(this);
-        if (!txn.getMutableTree(this).reclaim(loggable, loggables, expirationChecker) && !wasTreeCreated) {
+        if (!txn.getMutableTree(this).reclaim(loggable, loggables) && !wasTreeCreated) {
             txn.removeTreeMutable(this);
         }
     }
 
-    public ITree openImmutableTree(@NotNull final MetaTree metaTree) {
+    ITree openImmutableTree(@NotNull final MetaTree metaTree) {
         final int structureId = getStructureId();
         final long upToDateRootAddress = metaTree.getRootAddress(structureId);
         final boolean hasDuplicates = metaInfo.hasDuplicates();

environment/src/main/java/jetbrains/exodus/env/TemporaryEmptyStore.java
@@ -17,7 +17,6 @@ package jetbrains.exodus.env;
 
 import jetbrains.exodus.ByteIterable;
 import jetbrains.exodus.log.RandomAccessLoggable;
-import jetbrains.exodus.tree.IExpirationChecker;
 import jetbrains.exodus.tree.ITreeCursor;
 import jetbrains.exodus.tree.TreeMetaInfo;
 import org.jetbrains.annotations.NotNull;
@@ -93,8 +92,7 @@ class TemporaryEmptyStore extends StoreImpl {
     @Override
     public void reclaim(@NotNull final Transaction transaction,
                         @NotNull final RandomAccessLoggable loggable,
-                        @NotNull final Iterator<RandomAccessLoggable> loggables,
-                        @NotNull final IExpirationChecker expirationChecker) {
+                        @NotNull final Iterator<RandomAccessLoggable> loggables) {
         // nothing to reclaim
     }
 

environment/src/main/java/jetbrains/exodus/env/management/EnvironmentConfig.java
@@ -254,11 +254,6 @@ public class EnvironmentConfig extends MBeanBase implements EnvironmentConfigMBe
     }
 
     @Override
-    public boolean getGcUseExpirationChecker() {
-        return config.getGcUseExpirationChecker();
-    }
-
-    @Override
     public int getGcFileMinAge() {
         return config.getGcFileMinAge();
     }
@@ -311,6 +306,16 @@ public class EnvironmentConfig extends MBeanBase implements EnvironmentConfigMBe
     @Override
     public void setGcTransactionAcquireTimeout(int timeout) {
         config.setGcTransactionAcquireTimeout(timeout);
+    }
+
+    @Override
+    public int getGcTransactionTimeout() {
+        return config.getGcTransactionTimeout();
+    }
+
+    @Override
+    public void setGcTransactionTimeout(int timeout) {
+        config.setGcTransactionTimeout(timeout);
     }
 
     @Override

environment/src/main/java/jetbrains/exodus/env/management/EnvironmentConfigMBean.java
@@ -107,8 +107,6 @@ public interface EnvironmentConfigMBean {
 
     void setGcRenameFiles(boolean rename);
 
-    boolean getGcUseExpirationChecker();
-
     int getGcFileMinAge();
 
     void setGcFileMinAge(int minAge);
@@ -130,6 +128,10 @@ public interface EnvironmentConfigMBean {
     int getGcTransactionAcquireTimeout();
 
     void setGcTransactionAcquireTimeout(int timeout);
+
+    int getGcTransactionTimeout();
+
+    void setGcTransactionTimeout(int timeout);
 
     int getGcFilesDeletionDelay();
 

environment/src/main/java/jetbrains/exodus/gc/FileUtilization.java
@@ -1,95 +1,0 @@
-/**
- * Copyright 2010 - 2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package jetbrains.exodus.gc;
-
-import jetbrains.exodus.core.dataStructures.skiplists.LongIntSkipList;
-import jetbrains.exodus.log.ExpiredLoggableInfo;
-import jetbrains.exodus.log.Loggable;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.lang.ref.SoftReference;
-
-final class FileUtilization {
-
-    private long freeBytes;
-    private SoftReference<LongIntSkipList> freeSpace;
-
-    FileUtilization(long freeBytes) {
-        this.freeBytes = freeBytes;
-        freeSpace = new SoftReference<>(null);
-    }
-
-    FileUtilization() {
-        this(0);
-    }
-
-    long getFreeBytes() {
-        return freeBytes;
-    }
-
-    boolean isExpired(@NotNull final Loggable loggable) {
-        return isExpired(loggable.getAddress(), 1);
-    }
-
-    boolean isExpired(final long startAddress, final int length) {
-        final LongIntSkipList.SkipListNode node = getFreeSpace().getLessOrEqual(startAddress);
-        return node != null && node.getKey() + node.getValue() >= startAddress + length;
-    }
-
-    void fetchExpiredLoggable(@NotNull final ExpiredLoggableInfo loggable, @Nullable final LongIntSkipList freeSpace) {
-        final long address = loggable.address;
-        final int length = loggable.length;
-        freeBytes += length;
-        if (freeSpace != null) {
-            final LongIntSkipList.SkipListNode node = freeSpace.getLessOrEqual(address);
-            if (node == null) {
-                freeSpace.add(address, length);
-            } else {
-                final long freeSpaceStart = node.getKey();
-                final int freeSpaceLength = node.getValue();
-                if (freeSpaceStart + freeSpaceLength >= address) {
-                    node.setValue(Math.max(freeSpaceLength, (int) (address - freeSpaceStart + length)));
-                } else {
-                    freeSpace.add(address, length);
-                }
-            }
-            final LongIntSkipList.SkipListNode floorNode = freeSpace.getLessOrEqual(address);
-            if (floorNode != null) {
-                final LongIntSkipList.SkipListNode ceilingNode = freeSpace.getNext(floorNode);
-                if (ceilingNode != null) {
-                    final long leftStart = floorNode.getKey();
-                    final int leftLength = floorNode.getValue();
-                    final long nextStart = ceilingNode.getKey();
-                    if (leftStart + leftLength == nextStart) {
-                        floorNode.setValue(leftLength + ceilingNode.getValue());
-                        freeSpace.remove(nextStart);
-                    }
-                }
-            }
-        }
-    }
-
-    @NotNull
-    LongIntSkipList getFreeSpace() {
-        LongIntSkipList result = freeSpace.get();
-        if (result == null) {
-            result = new LongIntSkipList();
-            freeSpace = new SoftReference<>(result);
-        }
-        return result;
-    }
-}

environment/src/main/java/jetbrains/exodus/gc/GarbageCollector.java
@@ -25,7 +25,6 @@ import jetbrains.exodus.core.execution.JobProcessorAdapter;
 import jetbrains.exodus.env.*;
 import jetbrains.exodus.io.RemoveBlockType;
 import jetbrains.exodus.log.*;
-import jetbrains.exodus.tree.IExpirationChecker;
 import jetbrains.exodus.util.DeferredIO;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -58,8 +57,6 @@ public final class GarbageCollector {
     private final BackgroundCleaner cleaner;
     private volatile int newFiles; // number of new files appeared after last cleaning job
     @NotNull
-    private final IExpirationChecker expirationChecker;
-    @NotNull
     private final IntHashMap<StoreImpl> openStoresCache;
     private boolean useRegularTxn;
 
@@ -71,22 +68,6 @@ public final class GarbageCollector {
         utilizationProfile = new UtilizationProfile(env, this);
         cleaner = new BackgroundCleaner(this);
         newFiles = ec.getGcFilesInterval() + 1;
-        if (!ec.getGcUseExpirationChecker()) {
-            expirationChecker = IExpirationChecker.NONE;
-        } else {
-            expirationChecker = new IExpirationChecker() {
-
-                @Override
-                public boolean expired(@NotNull final Loggable loggable) {
-                    return utilizationProfile.isExpired(loggable);
-                }
-
-                @Override
-                public boolean expired(long startAddress, int length) {
-                    return utilizationProfile.isExpired(startAddress, length);
-                }
-            };
-        }
         openStoresCache = new IntHashMap<>();
         env.getLog().addNewFileListener(new NewFileListener() {
             @Override
@@ -138,10 +119,6 @@ public final class GarbageCollector {
 
     public long getFileFreeBytes(final long fileAddress) {
         return utilizationProfile.getFileFreeBytes(fileAddress);
-    }
-
-    public boolean isExpired(final long startAddress, final int length) {
-        return utilizationProfile.isExpired(startAddress, length);
     }
 
     public void suspend() {
@@ -374,7 +351,7 @@ public final class GarbageCollector {
                         store = txn.openStoreByStructureId(structureId);
                         openStoresCache.put(structureId, store);
                     }
-                    store.reclaim(txn, loggable, loggables, expirationChecker);
+                    store.reclaim(txn, loggable, loggables);
                 }
             }
         } catch (Throwable e) {

commit faeb702f730e85df059009d307ee37b7d9eb525b

fix weird reclaim bug introduced in 0dff56fdb2525803c950b97fd6d2469357075703

environment/src/main/java/jetbrains/exodus/tree/patricia/PatriciaTreeMutable.java
@@ -467,7 +467,7 @@ final class PatriciaTreeMutable extends PatriciaTreeBase implements ITreeMutable
                         actItr = actual.currentNode.keySequence.iterator();
                     }
                 } else if (actItr.hasNext()) {
-                    final NodeChildrenIterator children = sourceNode.getChildren(actItr.next());
+                    final NodeChildrenIterator children = source.currentNode.getChildren(actItr.next());
                     final ChildReference child = children.getNode();
                     if (child == null || !source.isAddressReclaimable(child.suffixAddress)) {
                         break; // child can be expired if source parent was already not-current
@@ -476,7 +476,7 @@ final class PatriciaTreeMutable extends PatriciaTreeBase implements ITreeMutable
                     source.currentIterator = children;
                     source.moveDown();
                     ++srcPushes;
-                    srcItr = sourceNode.keySequence.iterator();
+                    srcItr = source.currentNode.keySequence.iterator();
                 } else { // both iterators matched, here comes the branching
                     reclaimChildren(source, actual);
                     break;