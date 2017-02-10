d905ba22edc79ea6487864470ff57e3ed6cf6e42
Vyacheslav Lukianov <lvo@intellij.net>
2017-02-10 19:59:59 +0300

    Kotlin extensions for persistent data structures

---
+++ utils/src/main/kotlin/jetbrains/exodus/core/dataStructures/persistent/PersistentEx.kt
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2010 - 2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.exodus.core.dataStructures.persistent
+
+inline fun <K : Comparable<K>, V, R> Persistent23TreeMap<K, V>.read(block: Persistent23TreeMap.ImmutableMap<K, V>.() -> R): R {
+    return beginRead().block()
+}
+
+inline fun <K : Comparable<K>, V> Persistent23TreeMap<K, V>.write(block: Persistent23TreeMap.MutableMap<K, V>.() -> Unit): Boolean {
+    val mutableMap = beginWrite()
+    mutableMap.block()
+    return mutableMap.endWrite()
+}
+
+inline fun <K : Comparable<K>, V> Persistent23TreeMap<K, V>.writeFinally(block: Persistent23TreeMap.MutableMap<K, V>.() -> Unit) {
+    while (!write(block)) {
+    }
+}
+
+inline fun <V, R> PersistentLong23TreeMap<V>.read(block: PersistentLong23TreeMap<V>.ImmutableMap.() -> R): R {
+    return beginRead().block()
+}
+
+inline fun <V> PersistentLong23TreeMap<V>.write(block: PersistentLong23TreeMap<V>.MutableMap.() -> Unit): Boolean {
+    val mutableMap = beginWrite()
+    mutableMap.block()
+    return mutableMap.endWrite()
+}
+
+inline fun <V> PersistentLong23TreeMap<V>.writeFinally(block: PersistentLong23TreeMap<V>.MutableMap.() -> Unit) {
+    while (!write(block)) {
+    }
+}
+
+inline fun <K, R> PersistentHashSet<K>.read(block: PersistentHashSet.ImmutablePersistentHashSet<K>.() -> R): R {
+    return beginRead().block()
+}
+
+inline fun <K> PersistentHashSet<K>.write(block: PersistentHashSet.MutablePersistentHashSet<K>.() -> Unit): Boolean {
+    val mutableSet = beginWrite()
+    mutableSet.block()
+    return mutableSet.endWrite()
+}
+
+inline fun <K> PersistentHashSet<K>.writeFinally(block: PersistentHashSet.MutablePersistentHashSet<K>.() -> Unit) {
+    while (!write(block)) {
+    }
+}
+
+inline fun <K, V, R> PersistentHashMap<K, V>.read(block: PersistentHashMap<K, V>.ImmutablePersistentHashMap.() -> R): R {
+    return current.block()
+}
+
+inline fun <K, V> PersistentHashMap<K, V>.write(block: PersistentHashMap<K, V>.MutablePersistentHashMap.() -> Unit): Boolean {
+    val mutableMap = beginWrite()
+    mutableMap.block()
+    return mutableMap.endWrite()
+}
+
+inline fun <K, V> PersistentHashMap<K, V>.writeFinally(block: PersistentHashMap<K, V>.MutablePersistentHashMap.() -> Unit) {
+    while (!write(block)) {
+    }
+}
+
+inline fun <K, V, R> PersistentLinkedHashMap<K, V>.read(block: PersistentLinkedHashMap.PersistentLinkedHashMapMutable<K, V>.() -> R): R {
+    return beginWrite().block()
+}
+
+inline fun <K, V> PersistentLinkedHashMap<K, V>.write(block: PersistentLinkedHashMap.PersistentLinkedHashMapMutable<K, V>.() -> Unit): Boolean {
+    val mutableMap = beginWrite()
+    mutableMap.block()
+    return endWrite(mutableMap)
+}
+
+inline fun <K, V> PersistentLinkedHashMap<K, V>.writeFinally(block: PersistentLinkedHashMap.PersistentLinkedHashMapMutable<K, V>.() -> Unit) {
+    while (!write(block)) {
+    }
+}
