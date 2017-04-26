20a0f9827e23036de1dee7afb75673bf087ece98
Vyacheslav Lukianov <lvo@intellij.net>
2017-04-26 22:47:16 +0300

    avoid immediate caching of blob strings; #XD-595 fixed

--- openAPI/src/main/kotlin/jetbrains/exodus/entitystore/BlobStringsCache.kt
+++ openAPI/src/main/kotlin/jetbrains/exodus/entitystore/BlobStringsCache.kt
@@ -16,10 +16,14 @@
 package jetbrains.exodus.entitystore
 
 import jetbrains.exodus.core.dataStructures.SoftLongObjectCache
+import jetbrains.exodus.core.dataStructures.hash.HashUtil
+import java.lang.ref.SoftReference
+import java.util.*
 
 internal class BlobStringsCache {
 
     private val cache = SoftLongObjectCache<Pair<Int, String>>(BLOB_STRING_CACHE_SIZE)
+    private var bf = SoftReference<BitSet>(null)
 
     fun tryKey(blobVault: BlobVault, blobHandle: Long): String? {
         val cacheKey = cacheKey(blobVault, blobHandle)
@@ -35,10 +39,25 @@ internal class BlobStringsCache {
 
     fun cacheObject(blobVault: BlobVault, blobHandle: Long, value: String) {
         val cacheKey = cacheKey(blobVault, blobHandle)
-        cache.cacheObject(cacheKey, blobVault.identity to value)
+        val bit = (cacheKey % BLOOM_FILTER_SIZE).toInt()
+        val filter = bloomFilter
+        if (filter[bit]) {
+            cache.cacheObject(cacheKey, blobVault.identity to value)
+        } else {
+            filter[bit] = true
+        }
     }
 
     val hitRate: Float get() = cache.hitRate()
+
+    private val bloomFilter: BitSet get() {
+        var result = bf.get()
+        if (result == null) {
+            result = BitSet(BLOOM_FILTER_SIZE)
+            bf = SoftReference(result)
+        }
+        return result
+    }
 
     class BlobStringsCacheCreator {
 
@@ -48,6 +67,7 @@ internal class BlobStringsCache {
     private companion object {
 
         val BLOB_STRING_CACHE_SIZE: Int = Integer.getInteger("exodus.entityStore.blobStringsCacheSize", 0x1000)
+        val BLOOM_FILTER_SIZE = HashUtil.getCeilingPrime(BLOB_STRING_CACHE_SIZE)
 
         fun cacheKey(blobVault: BlobVault, blobHandle: Long) = blobHandle xor (blobVault.identity.toLong() shl 32)
     }
