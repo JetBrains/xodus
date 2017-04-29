67a978336c2a217cfbedaa5b91852f2db5798f77
Vyacheslav Lukianov <lvo@intellij.net>
2017-04-29 19:35:06 +0300

    UpdatablePropertiesCachedInstanceIterable rewritten in Kotlin

---
+++ entity-store/src/main/kotlin/jetbrains/exodus/entitystore/iterate/UpdatablePropertiesCachedInstanceIterable.kt
@@ -0,0 +1,210 @@
+/**
+ * Copyright 2010 - 2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.exodus.entitystore.iterate
+
+import jetbrains.exodus.core.dataStructures.persistent.Persistent23Tree
+import jetbrains.exodus.entitystore.EntityId
+import jetbrains.exodus.entitystore.EntityStoreException
+import jetbrains.exodus.entitystore.PersistentEntityId
+import jetbrains.exodus.entitystore.PersistentStoreTransaction
+import jetbrains.exodus.entitystore.tables.PropertyTypes
+import mu.KLogging
+
+class UpdatablePropertiesCachedInstanceIterable(txn: PersistentStoreTransaction?,
+                                                it: PropertyValueIterator?,
+                                                source: EntityIterableBase) : UpdatableCachedInstanceIterable(txn, source) {
+
+    companion object : KLogging() {
+
+        val PropertyValueIterator.nextId get() = this.nextId() ?: throw NullPointerException()
+        val PropertyValueIterator.currentValue: Comparable<Any> get() = this.currentValue() ?: throw NullPointerException()
+    }
+
+    private var typeId = -1
+    private var index = Persistent23Tree<IndexEntry>()
+    private var mutableIndex: Persistent23Tree.MutableTree<IndexEntry>? = null
+    private var valueClass: Class<Comparable<Any>>? = null
+
+    init {
+        try {
+            if (it != null && it.hasNext()) {
+                val id: EntityId = it.nextId
+                typeId = id.typeId
+                var prevValue: Comparable<Any> = it.currentValue
+                valueClass = prevValue.javaClass
+                val tempList = mutableListOf(IndexEntry(prevValue, id.localId))
+                while (it.hasNext()) {
+                    val localId = it.nextId.localId
+                    val currentValue: Comparable<Any> = it.currentValue
+                    if (prevValue == currentValue) {
+                        tempList.add(IndexEntry(prevValue, localId))
+                    } else {
+                        tempList.add(IndexEntry(currentValue, localId))
+                        prevValue = currentValue
+                        if (valueClass != currentValue.javaClass) {
+                            throw EntityStoreException("Unexpected property value class")
+                        }
+                    }
+                }
+                index.beginWrite().run {
+                    addAll(tempList, tempList.size)
+                    endWrite()
+                }
+            }
+        } finally {
+            if (it is EntityIteratorBase) {
+                it.disposeIfShouldBe()
+            }
+        }
+    }
+
+    // constructor for mutating source index
+    private constructor(source: UpdatablePropertiesCachedInstanceIterable) : this(source.transaction, null, source) {
+        typeId = source.typeId
+        index = source.index.clone
+        mutableIndex = index.beginWrite()
+        valueClass = source.valueClass
+    }
+
+    override fun getEntityTypeId() = typeId
+
+    fun getPropertyValueClass() = valueClass
+
+    override fun isSortedById() = false
+
+    override fun beginUpdate() = UpdatablePropertiesCachedInstanceIterable(this)
+
+    override fun isMutated() = mutableIndex != null
+
+    override fun endUpdate() {
+        val index = mutableIndex ?: throw IllegalStateException("UpdatablePropertiesCachedInstanceIterable was not mutated")
+        index.endWrite()
+        mutableIndex = null
+    }
+
+    fun update(typeId: Int, localId: Long, oldValue: Comparable<Any>?, newValue: Comparable<Any>?) {
+        if (this.typeId == -1) {
+            this.typeId = typeId
+        }
+        val oldEntry = if (oldValue == null) null else IndexEntry(PropertyTypes.toLowerCase(oldValue), localId)
+        val newEntry = if (newValue == null) null else IndexEntry(PropertyTypes.toLowerCase(newValue), localId)
+        if (oldEntry == null && newEntry == null) {
+            throw IllegalStateException("Can't update in-memory index: both oldValue and newValue are null")
+        }
+        val index = mutableIndex ?: throw IllegalStateException("Mutate index before updating it")
+        if (oldEntry != null) {
+            if (index.contains(oldEntry)) {
+                index.exclude(oldEntry)
+            } else if (newEntry != null && !index.contains(newEntry)) {
+                logger.warn("In-memory index doesn't contain the value [$oldValue]. New value [$newValue]. Handle [$handle]")
+            }
+        }
+        if (newEntry != null) {
+            index.add(newEntry)
+            if (valueClass == null) {
+                valueClass = newEntry.propValue.javaClass
+            }
+        }
+    }
+
+    fun getPropertyValueIterator(value: Comparable<Any>): EntityIteratorBase {
+        return PropertyValueCachedInstanceIterator(value)
+    }
+
+    fun getPropertyRangeIterator(min: Comparable<Any>, max: Comparable<Any>): EntityIteratorBase {
+        return PropertyRangeCachedInstanceIterator(min, max)
+    }
+
+    override fun getIteratorImpl(txn: PersistentStoreTransaction): EntityIteratorBase {
+        return if (currentTree.isEmpty) EntityIteratorBase.EMPTY else PropertiesCachedInstanceIterator()
+    }
+
+    override fun getReverseIteratorImpl(txn: PersistentStoreTransaction): EntityIteratorBase {
+        return if (currentTree.isEmpty) EntityIteratorBase.EMPTY else ReversePropertiesCachedInstanceIterator()
+    }
+
+    override fun size() = currentTree.size().toLong()
+
+    override fun countImpl(txn: PersistentStoreTransaction) = size()
+
+    private val currentTree get() = mutableIndex ?: index.beginRead()
+
+    private class IndexEntry(val propValue: Comparable<Any>, val localId: Long) : Comparable<IndexEntry> {
+
+        override fun compareTo(other: IndexEntry): Int {
+            var result = propValue.compareTo(other.propValue)
+            if (result == 0) {
+                if (localId < other.localId) {
+                    result = -1
+                } else if (localId > other.localId) {
+                    result = 1
+                }
+            }
+            return result
+        }
+    }
+
+    private abstract inner class PropertiesCachedInstanceIteratorBase(private val it: Iterator<IndexEntry>)
+        : NonDisposableEntityIterator(this@UpdatablePropertiesCachedInstanceIterable), PropertyValueIterator {
+
+        protected var next: IndexEntry? = null
+        protected var hasNextValid: Boolean = false
+
+        override fun hasNextImpl(): Boolean {
+            if (!hasNextValid) {
+                if (!it.hasNext()) {
+                    return false
+                }
+                val next = it.next()
+                if (!checkIndexEntry(next)) {
+                    return false
+                }
+                this.next = next
+                hasNextValid = true
+            }
+            return true
+        }
+
+        public override fun nextIdImpl(): EntityId? {
+            if (!hasNextImpl()) {
+                return null
+            }
+            val result = PersistentEntityId(entityTypeId, next!!.localId)
+            hasNextValid = false
+            return result
+        }
+
+        override fun currentValue() = next?.propValue
+
+        protected open fun checkIndexEntry(entry: IndexEntry?) = entry != null
+    }
+
+    private inner class PropertiesCachedInstanceIterator : PropertiesCachedInstanceIteratorBase(currentTree.iterator())
+
+    private inner class ReversePropertiesCachedInstanceIterator : PropertiesCachedInstanceIteratorBase(currentTree.reverseIterator())
+
+    private inner class PropertyValueCachedInstanceIterator(private val value: Comparable<Any>)
+        : PropertiesCachedInstanceIteratorBase(currentTree.tailIterator(IndexEntry(value, 0))) {
+
+        override fun checkIndexEntry(entry: IndexEntry?) = entry != null && entry.propValue == value
+    }
+
+    private inner class PropertyRangeCachedInstanceIterator(min: Comparable<Any>, private val max: Comparable<Any>)
+        : PropertiesCachedInstanceIteratorBase(currentTree.tailIterator(IndexEntry(min, 0))) {
+
+        override fun checkIndexEntry(entry: IndexEntry?) = entry != null && entry.propValue <= max
+    }
+}
