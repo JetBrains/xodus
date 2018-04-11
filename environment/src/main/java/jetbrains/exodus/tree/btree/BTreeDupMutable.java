08d90f1a26b0932e1701ab64d44ecf24e2d494dc
Pavel Nikitin <pavel.nikitin@jetbrains.com>
2018-04-11 11:24:57 +0200

    fail-fast for suspicious BTree behavior

--- environment/src/main/java/jetbrains/exodus/tree/btree/BTreeDupMutable.java
+++ environment/src/main/java/jetbrains/exodus/tree/btree/BTreeDupMutable.java
@@ -83,12 +83,17 @@ final class BTreeDupMutable extends BTreeMutable {
         } else {
             canRetry = true;
         }
-        if (!log.isLastWrittenFileAddress(startAddress) && NullLoggable.isNullLoggable(log.getWrittenLoggableType(startAddress))) {
-            final long lengthBound = log.getFileLengthBound();
-            final long alignment = startAddress % lengthBound;
-            startAddress += (lengthBound - alignment);
-            if (log.getWrittenHighAddress() < startAddress) {
-                throw new IllegalStateException("Address alignment underflow: start address " + startAddress + ", alignment " + alignment);
+        if (!log.isLastWrittenFileAddress(startAddress)) {
+            final byte writtenType = log.getWrittenLoggableType(startAddress);
+            if (NullLoggable.isNullLoggable(writtenType)) {
+                final long lengthBound = log.getFileLengthBound();
+                final long alignment = startAddress % lengthBound;
+                startAddress += (lengthBound - alignment);
+                if (log.getWrittenHighAddress() < startAddress) {
+                    throw new IllegalStateException("Address alignment underflow: start address " + startAddress + ", alignment " + alignment);
+                }
+            } else if (writtenType > BTreeBase.DUP_LEAF) {
+                throw new IllegalStateException("Unknown written loggable type: " + writtenType);
             }
         }
         sizeIterable = CompressedUnsignedLongByteIterable.getIterable((size << 1) + 1);
