56ab99df3bf6f53cddbae4ba8ce6d83e9c8091de
lehvolk <alexey.volkov@jetbrains.com>
2018-05-22 14:25:10 +0300

    XD-711 Add S3 data reader

---
+++ multinode/src/main/kotlin/jetbrains/exodus/log/replication/S3DataReader.kt
@@ -0,0 +1,222 @@
+/**
+ * Copyright 2010 - 2018 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.exodus.log.replication
+
+import jetbrains.exodus.io.Block
+import jetbrains.exodus.io.DataReader
+import jetbrains.exodus.io.RemoveBlockType
+import jetbrains.exodus.log.LogUtil
+import mu.KLogging
+import software.amazon.awssdk.core.AwsRequestOverrideConfig
+import software.amazon.awssdk.services.s3.S3AsyncClient
+import software.amazon.awssdk.services.s3.model.*
+import java.util.*
+import kotlin.Comparator
+
+
+class S3DataReader(
+        val s3: S3AsyncClient,
+        val bucketName: String,
+        val requestOverrideConfig: AwsRequestOverrideConfig? = null) : DataReader {
+
+    companion object : KLogging()
+
+    private val fileBlocks: List<S3Block> get() = blocks.sortedBy { it._address }.toList()
+    private val folderBlocks: List<S3FolderBlock> get() = subBlocks.sortedBy { it._address }.toList()
+
+    @Suppress("UNCHECKED_CAST")
+    override fun getBlocks(): Iterable<Block> {
+        val result = TreeSet<Block>(Comparator<Block> { o1, o2 ->
+            o1.address.compareTo(o2.address)
+        })
+        return result.apply {
+            addAll(fileBlocks)
+            addAll(folderBlocks)
+        }
+    }
+
+    override fun getBlocks(fromAddress: Long): Iterable<Block> = blocks.filter { it.address >= fromAddress }.toList()
+
+    override fun getLocation(): String = "s3:$bucketName"
+
+    override fun removeBlock(blockAddress: Long, rbt: RemoveBlockType) {
+        // TODO
+    }
+
+    override fun truncateBlock(blockAddress: Long, length: Long) {
+        TODO("not implemented")
+    }
+
+    override fun close() = s3.close()
+
+    override fun getBlock(address: Long): Block {
+        logger.info { "Get block at ${LogUtil.getLogFilename(address)}" }
+        return blocks.first { it.address == address }
+    }
+
+    override fun clear() {
+        val builder = listObjectsBuilder()
+        while (true) {
+            val response = s3.listObjects(builder.build()).get()
+            response.contents()?.let {
+                s3.deleteObjects(DeleteObjectsRequest.builder()
+                        .requestOverrideConfig(requestOverrideConfig)
+                        .delete(
+                                Delete.builder().objects(
+                                        it
+                                                .filter { it.key().isValidAddress || it.key().isValidSubFolder }
+                                                .map { ObjectIdentifier.builder().key(it.key()).build() })
+                                        .build())
+                        .bucket(bucketName)
+                        .build()).get()
+            }
+            if (!response.isTruncated) {
+                break
+            }
+            builder.marker(response.marker())
+        }
+    }
+
+    internal inner class S3Block(val _address: Long, private val s3Object: S3Object) : Block {
+
+        override fun getAddress() = _address
+
+        override fun setReadOnly() = true
+
+        override fun length(): Long = s3Object.size()
+
+        override fun read(output: ByteArray, position: Long, offset: Int, count: Int): Int {
+            if (count <= 0) {
+                return 0
+            }
+            val range = "bytes=$position-${position + count - 1}"
+
+            logger.debug { "Request range: $range" }
+            val written = s3.getObject(GetObjectRequest.builder()
+                    .range(range)
+                    .requestOverrideConfig(requestOverrideConfig)
+                    .bucket(bucketName)
+                    .key(s3Object.key()).build(),
+                    ByteArrayAsyncResponseHandler(output, offset)
+            ).get()
+
+            if (written < count) {
+                logger.debug { "Read underflow: expected $count, got $written" }
+            }
+
+            return written
+        }
+    }
+
+    internal inner class S3FolderBlock(val _address: Long, private val folderName: String) : Block {
+
+        private val blocks by lazy {
+            val builder = listObjectsBuilder().prefix("_$folderName/")
+            s3Objects(builder) { it.key().isValidSubFolder }
+                    .asSequence()
+                    .map {
+                        S3Block(it.key().split("/")[1].address, it)
+                    }
+                    .sortedBy { it._address }
+                    .toList()
+        }
+
+        override fun getAddress() = _address
+
+        override fun setReadOnly() = true
+
+        override fun length(): Long = blocks.fold(0L) { acc, value -> acc + value.length() }
+
+        override fun read(output: ByteArray, position: Long, offset: Int, count: Int): Int {
+            if (count <= 0) {
+                return 0
+            }
+            val firstIndex = blocks.indexOfFirst { it.address + it.length() >= position }
+            val lastIndex = blocks.indexOfFirst { it.address + it.length() <= position + count }
+
+            val first = blocks[firstIndex]
+            val last = blocks[lastIndex]
+            var written = 0
+            written += first.read(output, first.address, offset, first.length().toInt())
+            written += last.read(output, last.address, offset + count, last.length().toInt())
+            if (firstIndex + 1 > lastIndex) {
+                ((firstIndex + 1)..lastIndex).forEach {
+                    val block = blocks[it]
+                    written += block.read(output, block.address, offset + (block.address - position).toInt(), block.length().toInt())
+                }
+            }
+            return written
+        }
+    }
+
+    private val blocks: List<S3Block>
+        get() {
+            val builder = listObjectsBuilder().delimiter("/")
+            return s3Objects(builder) { it.key().isValidAddress }
+                    .asSequence()
+                    .map { S3Block(it.key().address, it) }
+                    .sortedBy { it._address }
+                    .toList()
+        }
+
+    private val subBlocks: List<S3FolderBlock>
+        get() {
+            val builder = listObjectsBuilder().prefix("_")
+            return s3Objects(builder) { it.key().isValidSubFolder }
+                    .asSequence()
+                    .map {
+                        val folderName = it.key().split("/")[0].drop(1)
+                        S3FolderBlock(folderName.toFileName().address, folderName)
+                    }
+                    .sortedBy { it._address }
+                    .toList()
+        }
+
+    private fun listObjectsBuilder(): ListObjectsRequest.Builder {
+        return ListObjectsRequest.builder()
+                .requestOverrideConfig(requestOverrideConfig)
+                .bucket(bucketName)
+    }
+
+    private fun s3Objects(builder: ListObjectsRequest.Builder, filter: (S3Object) -> Boolean): List<S3Object> {
+        val result = LinkedList<S3Object>()
+        while (true) {
+            val response = s3.listObjects(builder.build()).get()
+            response.contents()?.let {
+                result.addAll(it.filter(filter))
+            }
+            if (!response.isTruncated) {
+                break
+            }
+            builder.marker(response.marker())
+        }
+        return result
+    }
+
+    private fun String.toFileName() = this + LogUtil.LOG_FILE_EXTENSION
+    private val String.address get() = LogUtil.getAddress(this)
+    private val String.isValidAddress get() = this.length == LogUtil.LOG_FILE_NAME_WITH_EXT_LENGTH && LogUtil.isLogFileName(this)
+
+    private val String.isValidSubFolder: Boolean
+        get() {
+            val paths = this.split("/")
+            val isValidFolderName = paths[0].let {
+                it.startsWith("_") && it.drop(1).toFileName().isValidAddress
+            }
+            return paths.size == 2 && paths[1].isValidAddress && isValidFolderName
+        }
+
+}
---
+++ multinode/src/test/kotlin/jetbrains/exodus/log/replication/S3DataReaderTest.kt
@@ -0,0 +1,174 @@
+package jetbrains.exodus.log.replication
+
+
+import io.findify.s3mock.S3Mock
+import jetbrains.exodus.TestUtil
+import jetbrains.exodus.log.LogUtil.LOG_BLOCK_ALIGNMENT
+import jetbrains.exodus.log.LogUtil.getLogFilename
+import mu.KLogging
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import software.amazon.awssdk.core.AwsRequestOverrideConfig
+import software.amazon.awssdk.core.auth.AnonymousCredentialsProvider
+import software.amazon.awssdk.core.client.builder.ClientAsyncHttpConfiguration
+import software.amazon.awssdk.core.regions.Region
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient
+import software.amazon.awssdk.http.nio.netty.NettySdkHttpClientFactory
+import software.amazon.awssdk.services.s3.S3AdvancedConfiguration
+import software.amazon.awssdk.services.s3.S3AsyncClient
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URI
+
+class S3DataReaderTest {
+    companion object : KLogging() {
+        const val port = 8001
+        const val host = "127.0.0.1"
+        const val bucket = "logfiles"
+    }
+
+    private lateinit var api: S3Mock
+    protected lateinit var httpClient: SdkAsyncHttpClient
+    protected lateinit var s3: S3AsyncClient
+    protected lateinit var extraHost: AwsRequestOverrideConfig
+
+    private val sourceDir by lazy { newTmpFile() }
+
+    private fun newTmpFile(): File {
+        return File(TestUtil.createTempDir(), bucket).also {
+            it.mkdirs()
+        }
+    }
+
+    @Before
+    fun setup() {
+        api = S3Mock.Builder().withPort(port).withFileBackend(sourceDir.parentFile.absolutePath).build().apply {
+            start()
+        }
+
+        httpClient = NettySdkHttpClientFactory.builder().build().createHttpClient()
+
+        extraHost = AwsRequestOverrideConfig.builder().header("Host", "$host:$port").build()
+
+        s3 = S3AsyncClient.builder()
+                .asyncHttpConfiguration(
+                        ClientAsyncHttpConfiguration.builder().httpClient(httpClient).build()
+                )
+                .region(Region.US_WEST_2)
+                .endpointOverride(URI("http://$host:$port"))
+                .advancedConfiguration(
+                        S3AdvancedConfiguration.builder().pathStyleAccessEnabled(true).build()  // for tests only
+                )
+                .credentialsProvider(AnonymousCredentialsProvider.create())
+                .build()
+    }
+
+    @After
+    fun cleanup() {
+        if (::api.isInitialized) {
+            api.shutdown()
+        }
+        if (::s3.isInitialized) {
+            s3.close()
+        }
+        if (::httpClient.isInitialized) {
+            httpClient.close()
+        }
+        sourceDir.delete()
+    }
+
+    @Test
+    fun `should read simple xd-files`() {
+        sourceDir.newDBFile(0)
+        sourceDir.newDBFile(1)
+        sourceDir.newDBFile(2, 100) // total length 356
+        with(newReader()) {
+            val blocks = blocks.toList()
+            assertEquals(3, blocks.size)
+            assertEquals(0, blocks[0].address.toInt())
+            assertEquals(LOG_BLOCK_ALIGNMENT, blocks[1].address.toInt())
+            assertEquals(2 * LOG_BLOCK_ALIGNMENT, blocks[2].address.toInt())
+        }
+    }
+
+    @Test
+    fun `should read xd-files with partiall folders`() {
+        sourceDir.newDBFile(0)
+        sourceDir.newDBFile(1)
+        newDBFolder(2, 100) // total length 356
+        with(newReader()) {
+            val blocks = blocks.toList()
+            assertEquals(3, blocks.size)
+            assertEquals(0, blocks[0].address.toInt())
+            assertEquals(LOG_BLOCK_ALIGNMENT, blocks[1].address.toInt())
+            assertEquals(2 * LOG_BLOCK_ALIGNMENT, blocks[2].address.toInt())
+        }
+    }
+
+    @Test
+    fun `xd-files should have higher priority then partially folders`() {
+        sourceDir.newDBFile(0)
+        sourceDir.newDBFile(1)
+        sourceDir.newDBFile(2)
+        newDBFolder(1)
+        with(newReader()) {
+            val blocks = blocks.toList()
+            assertEquals(3, blocks.size)
+            assertEquals(0, blocks[0].address.toInt())
+            assertEquals(LOG_BLOCK_ALIGNMENT, blocks[1].address.toInt())
+            assertEquals(2 * LOG_BLOCK_ALIGNMENT, blocks[2].address.toInt())
+            assertTrue(blocks.all { it is S3DataReader.S3Block })
+        }
+    }
+
+    @Test
+    fun `should handle partially files`() {
+        sourceDir.newDBFile(0)
+        sourceDir.newDBFile(1)
+        newDBFolder(2, 100) // total length 356
+        with(newReader()) {
+            val blocks = blocks.toList()
+            assertEquals(3, blocks.size)
+            assertEquals(0, blocks[0].address.toInt())
+            assertEquals(LOG_BLOCK_ALIGNMENT, blocks[1].address.toInt())
+            assertEquals(2 * LOG_BLOCK_ALIGNMENT, blocks[2].address.toInt())
+        }
+    }
+
+    @Test
+    fun `should delete files on close`() {
+        sourceDir.newDBFile(0)
+        newDBFolder(1, 100)
+        with(newReader()) {
+            clear()
+            assertTrue(sourceDir.listFiles().none { it.name.endsWith(".xd") })
+        }
+    }
+
+
+    private fun File.newDBFile(number: Long, size: Int = 128): String {
+        val file = File(this, getLogFilename(LOG_BLOCK_ALIGNMENT * number)).also { it.createNewFile() }
+        FileOutputStream(file).use {
+            it.write(ByteArray(size) { 1 })
+        }
+        return file.name
+    }
+
+    private fun newDBFolder(number: Long, size: Int = 128): String {
+        val file = sourceDir.let {
+            val name = getLogFilename(LOG_BLOCK_ALIGNMENT * number).replace(".xd", "")
+            File(it.absolutePath + File.separator + "_" + name).also {
+                it.mkdir()
+                it.newDBFile(LOG_BLOCK_ALIGNMENT * number, size)
+                it.newDBFile(LOG_BLOCK_ALIGNMENT * (number + 1), size)
+            }
+        }
+        return file.name
+    }
+
+    private fun newReader() = S3DataReader(s3, bucket, extraHost)
+
+}
