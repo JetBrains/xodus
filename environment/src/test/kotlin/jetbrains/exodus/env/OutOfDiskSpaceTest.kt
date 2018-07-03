45eafc9a00437e5f293de6702dccd49bf73167ed
Vyacheslav Lukianov <lvo@intellij.net>
2018-07-03 20:20:04 +0300

    more tests, actually #XD-733 fixed

--- environment/src/test/kotlin/jetbrains/exodus/env/OutOfDiskSpaceTest.kt
+++ environment/src/test/kotlin/jetbrains/exodus/env/OutOfDiskSpaceTest.kt
@@ -15,11 +15,13 @@
  */
 package jetbrains.exodus.env
 
+import jetbrains.exodus.ArrayByteIterable
 import jetbrains.exodus.ExodusException
 import jetbrains.exodus.TestFor
 import jetbrains.exodus.bindings.IntegerBinding
 import jetbrains.exodus.bindings.StringBinding
 import jetbrains.exodus.log.LogTestConfig
+import jetbrains.exodus.log.TooBigLoggableException
 import org.junit.Assert
 import org.junit.Test
 
@@ -27,7 +29,62 @@ open class OutOfDiskSpaceTest : EnvironmentTestsBase() {
 
     @Test
     @TestFor(issues = ["XD-733"])
-    fun testOODS() {
+    fun `emulate out of disk space`() {
+        val (store0, store1) = prepareStores()
+
+        val highAddress = log.highAddress
+        for (l in highAddress * 2 downTo highAddress) {
+            val logTestConfig = LogTestConfig().apply {
+                maxHighAddress = l
+            }
+            env.log.setLogTestConfig(logTestConfig)
+            try {
+                env.executeInTransaction { txn ->
+                    for (i in 10..100) {
+                        store0.put(txn, IntegerBinding.intToEntry(i), StringBinding.stringToEntry(i.toString()))
+                        store1.put(txn, StringBinding.stringToEntry(i.toString()), IntegerBinding.intToEntry(i))
+                    }
+                }
+            } catch (e: ExodusException) {
+            }
+            env.log.setLogTestConfig(null)
+            env.executeInTransaction { txn ->
+                store0.put(txn, StringBinding.stringToEntry(" "), ArrayByteIterable(ByteArray(4)))
+            }
+            assert(store0, store1)
+            env.executeInTransaction { txn ->
+                Assert.assertEquals(ArrayByteIterable(ByteArray(4)), store0[txn, StringBinding.stringToEntry(" ")])
+            }
+        }
+    }
+
+    @Test
+    @TestFor(issues = ["XD-733"])
+    fun `too big loggable`() {
+        val (store0, store1) = prepareStores()
+        println(env.log.highAddress)
+        for (i in 0 until 10) {
+            try {
+                env.executeInTransaction { txn ->
+                    store0.put(txn, StringBinding.stringToEntry(" "), ArrayByteIterable(ByteArray(1024)))
+                }
+            } catch (e: TooBigLoggableException) {
+                assert(store0, store1)
+                env.executeInTransaction { txn ->
+                    store0.put(txn, StringBinding.stringToEntry(" "), ArrayByteIterable(ByteArray(128)))
+                }
+                assert(store0, store1)
+                env.executeInTransaction { txn ->
+                    Assert.assertEquals(ArrayByteIterable(ByteArray(128)), store0[txn, StringBinding.stringToEntry(" ")])
+                }
+                println(env.log.highAddress)
+                continue
+            }
+            Assert.fail()
+        }
+    }
+
+    private fun prepareStores(): Pair<Store, Store> {
         env.environmentConfig.logCachePageSize = 1024
         env.environmentConfig.logFileSize = 1
         invalidateSharedCaches()
@@ -46,30 +103,16 @@ open class OutOfDiskSpaceTest : EnvironmentTestsBase() {
                 store1.put(txn, StringBinding.stringToEntry(i.toString()), IntegerBinding.intToEntry(i))
             }
         }
-        val highAddress = log.highAddress
-        for (l in highAddress * 2 downTo highAddress) {
-            val logTestConfig = LogTestConfig().apply {
-                maxHighAddress = l
+        return store0 to store1
+    }
+
+    private fun assert(store0: Store, store1: Store) {
+        for (i in 0..9) {
+            env.executeInTransaction { txn ->
+                Assert.assertEquals(StringBinding.stringToEntry(i.toString()), store0[txn, IntegerBinding.intToEntry(i)])
             }
-            env.log.setLogTestConfig(logTestConfig)
-            try {
-                env.executeInTransaction { txn ->
-                    for (i in 10..100) {
-                        store0.put(txn, IntegerBinding.intToEntry(i), StringBinding.stringToEntry(i.toString()))
-                        store1.put(txn, StringBinding.stringToEntry(i.toString()), IntegerBinding.intToEntry(i))
-                    }
-                }
-            } catch (e: ExodusException) {
-            }
-            Assert.assertEquals(highAddress, log.highAddress)
-            env.log.setLogTestConfig(null)
-            for (i in 0..9) {
-                env.executeInTransaction { txn ->
-                    Assert.assertEquals(StringBinding.stringToEntry(i.toString()), store0[txn, IntegerBinding.intToEntry(i)])
-                }
-                env.executeInTransaction { txn ->
-                    Assert.assertEquals(IntegerBinding.intToEntry(i), store1[txn, StringBinding.stringToEntry(i.toString())])
-                }
+            env.executeInTransaction { txn ->
+                Assert.assertEquals(IntegerBinding.intToEntry(i), store1[txn, StringBinding.stringToEntry(i.toString())])
             }
         }
     }
