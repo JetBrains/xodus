JetBrains/xodus

commit 14072c3837b09302dbd9b259fe4a524ea857c724

speed up FixedLengthByteIterable.getBytesUnsafe() if source is ByteIterableBase

openAPI/src/main/java/jetbrains/exodus/FixedLengthByteIterable.java
@@ -39,11 +39,21 @@ public class FixedLengthByteIterable extends ByteIterableBase {
     public byte[] getBytesUnsafe() {
         if (bytes == null) {
             final int length = this.length;
-            final byte[] bytes = new byte[length];
-            final ByteIterator it = source.iterator();
-            it.skip(offset);
-            for (int i = 0; it.hasNext() && i < length; ++i) {
-                bytes[i] = it.next();
+            byte[] bytes = null;
+            if (source instanceof ByteIterableBase) {
+                final ByteIterableBase s = (ByteIterableBase) source;
+                if (s.bytes != null) {
+                    bytes = new byte[length];
+                    System.arraycopy(s.bytes, offset, bytes, 0, length);
+                }
+            }
+            if (bytes == null) {
+                bytes = new byte[length];
+                final ByteIterator it = source.iterator();
+                it.skip(offset);
+                for (int i = 0; it.hasNext() && i < length; ++i) {
+                    bytes[i] = it.next();
+                }
             }
             this.bytes = bytes;
         }

commit 5051548e8fe2b3c6353cbeea94f7fec332ddbc7b

fix FiledLengthByteIterable#getBytesUnsafe() broken by 14072c3837b09302dbd9b259fe4a524ea857c724

openAPI/src/main/java/jetbrains/exodus/FixedLengthByteIterable.java
@@ -42,9 +42,10 @@ public class FixedLengthByteIterable extends ByteIterableBase {
             byte[] bytes = null;
             if (source instanceof ByteIterableBase) {
                 final ByteIterableBase s = (ByteIterableBase) source;
-                if (s.bytes != null) {
+                final byte[] sourceBytes = s.getBytesUnsafe();
+                if (sourceBytes != null) {
                     bytes = new byte[length];
-                    System.arraycopy(s.bytes, offset, bytes, 0, length);
+                    System.arraycopy(sourceBytes, offset, bytes, 0, length);
                 }
             }
             if (bytes == null) {

environment/src/test/kotlin/jetbrains/exodus/env/StoreTest.kt
@@ -389,6 +389,23 @@ class StoreTest : EnvironmentTestsBase() {
         }
     }
 
+    @Test
+    fun `FixedLengthByteIterable#getBytesUnsafe`() {
+        val store = openStoreAutoCommit("store", StoreConfig.WITHOUT_DUPLICATES)
+        env.executeInTransaction { txn ->
+            store.put(txn, key, value)
+        }
+        env.executeInTransaction { txn ->
+            assertEquals(value, store[txn, key])
+        }
+        env.executeInTransaction { txn ->
+            val vl = value.length
+            (0 until vl).forEach { i ->
+                assertEquals(value.subIterable(i, vl - i), store[txn, key]?.subIterable(i, vl - i))
+            }
+        }
+    }
+
     private fun putWithoutDuplicates(config: StoreConfig) {
         val env = environment
         var txn: Transaction = env.beginTransaction()