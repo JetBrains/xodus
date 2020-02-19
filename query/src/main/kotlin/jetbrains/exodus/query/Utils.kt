c4751fde800a41e741d353e514d6645646e9a3c8
Vyacheslav Lukianov <lvo@intellij.net>
2020-02-19 13:13:09 +0300

    by default, jetbrains.exodus.query.reduceUnionsOfLinksDepth = 20

--- query/src/main/kotlin/jetbrains/exodus/query/Utils.kt
+++ query/src/main/kotlin/jetbrains/exodus/query/Utils.kt
@@ -24,7 +24,7 @@ internal object Utils {
     @JvmStatic
     val unionSubtypes = parseBoolean(System.getProperty("jetbrains.exodus.query.unionSubtypesResults", "true"))
     @JvmStatic
-    val reduceUnionsOfLinksDepth: Int = getInteger("jetbrains.exodus.query.reduceUnionsOfLinksDepth", 50)
+    val reduceUnionsOfLinksDepth: Int = getInteger("jetbrains.exodus.query.reduceUnionsOfLinksDepth", 20)
 
     @JvmStatic
     fun safe_equals(left: Any?, right: Any?) = if (left != null) left == right else right == null
