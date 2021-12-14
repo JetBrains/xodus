6fa87007162220ba423bc4259e992cc72769ebb1
Vyacheslav Lukianov <lvo@jetbrains.com>
2021-12-14 14:39:09 +0300

    non-trivial MinusIterable.getReverseIteratorImpl()

--- entity-store/src/main/java/jetbrains/exodus/entitystore/iterate/binop/MinusIterable.java
+++ entity-store/src/main/java/jetbrains/exodus/entitystore/iterate/binop/MinusIterable.java
@@ -48,54 +48,71 @@ public final class MinusIterable extends BinaryOperatorEntityIterable {
         final EntityIterableBase iterable1 = this.iterable1;
         final EntityIterableBase iterable2 = this.iterable2;
         return new EntityIteratorFixingDecorator(this, isSortedById() && iterable2.isSortedById() ?
-                new SortedIterator(this, iterable1, iterable2) : new UnsortedIterator(this, txn, iterable1, iterable2));
+            new SortedIterator(this, iterable1, iterable2) : new UnsortedIterator(this, txn, iterable1, iterable2));
     }
 
-    private static final class SortedIterator extends NonDisposableEntityIterator {
+    @Override
+    public @NotNull EntityIterator getReverseIteratorImpl(@NotNull final PersistentStoreTransaction txn) {
+        if (isSortedById()) {
+            return new EntityIteratorFixingDecorator(this,
+                new SortedReverseIterator(this, txn, iterable1, iterable2));
+        }
+        return super.getReverseIteratorImpl(txn);
+    }
 
-        @NotNull
-        private final EntityIteratorBase minuend;
-        private final EntityIteratorBase subtrahend;
-        private EntityId nextId;
-        private EntityId currentMinuend;
-        private EntityId currentSubtrahend;
+    private static abstract class SortedIteratorBase extends NonDisposableEntityIterator {
 
-        private SortedIterator(@NotNull final EntityIterableBase iterable,
-                               @NotNull final EntityIterableBase minuend,
-                               @NotNull final EntityIterableBase subtrahend) {
+        protected EntityId nextId;
+        protected EntityId currentMinuend;
+        protected EntityId currentSubtrahend;
+
+        protected SortedIteratorBase(@NotNull final EntityIterableBase iterable) {
             super(iterable);
-            this.minuend = (EntityIteratorBase) minuend.iterator();
-            this.subtrahend = (EntityIteratorBase) subtrahend.iterator();
             nextId = null;
             currentMinuend = null;
             currentSubtrahend = null;
         }
 
         @Override
+        @Nullable
+        public EntityId nextIdImpl() {
+            return nextId;
+        }
+    }
+
+    private static final class SortedIterator extends SortedIteratorBase {
+
+        private final EntityIteratorBase minuend;
+        private final EntityIteratorBase subtrahend;
+
+        SortedIterator(@NotNull final EntityIterableBase iterable,
+                       @NotNull final EntityIterableBase minuend,
+                       @NotNull final EntityIterableBase subtrahend) {
+            super(iterable);
+            this.minuend = (EntityIteratorBase) minuend.iterator();
+            this.subtrahend = (EntityIteratorBase) subtrahend.iterator();
+        }
+
+        @Override
         protected boolean hasNextImpl() {
             EntityId currentMinuend = this.currentMinuend;
             EntityId currentSubtrahend = this.currentSubtrahend;
-
             while (currentMinuend != PersistentEntityId.EMPTY_ID) {
-
                 if (currentMinuend == null) {
-                    this.currentMinuend = currentMinuend = nextId(minuend);
+                    this.currentMinuend = currentMinuend = checkNextId(minuend);
                 }
                 if (currentSubtrahend == null) {
-                    this.currentSubtrahend = currentSubtrahend = nextId(subtrahend);
+                    this.currentSubtrahend = currentSubtrahend = checkNextId(subtrahend);
                 }
-
                 // no more ids in minuend
                 if ((nextId = currentMinuend) == PersistentEntityId.EMPTY_ID) {
                     break;
                 }
-
                 // no more ids subtrahend
                 if (currentSubtrahend == PersistentEntityId.EMPTY_ID) {
                     currentMinuend = null;
                     break;
                 }
-
                 if (currentMinuend != currentSubtrahend && (currentMinuend == null || currentSubtrahend == null)) {
                     break;
                 }
@@ -117,17 +134,69 @@ public final class MinusIterable extends BinaryOperatorEntityIterable {
             this.currentSubtrahend = currentSubtrahend;
             return this.currentMinuend != PersistentEntityId.EMPTY_ID;
         }
+    }
+
+    private static final class SortedReverseIterator extends SortedIteratorBase {
+
+        private final EntityIteratorBase minuend;
+        private final EntityIteratorBase subtrahend;
+
+
+        SortedReverseIterator(@NotNull final EntityIterableBase iterable,
+                              @NotNull final PersistentStoreTransaction txn,
+                              @NotNull final EntityIterableBase minuend,
+                              @NotNull final EntityIterableBase subtrahend) {
+            super(iterable);
+            this.minuend = (EntityIteratorBase) minuend.getReverseIteratorImpl(txn);
+            this.subtrahend = (EntityIteratorBase) subtrahend.getReverseIteratorImpl(txn);
+        }
 
         @Override
-        @Nullable
-        public EntityId nextIdImpl() {
-            return nextId;
+        protected boolean hasNextImpl() {
+            EntityId currentMinuend = this.currentMinuend;
+            EntityId currentSubtrahend = this.currentSubtrahend;
+            while (currentMinuend != PersistentEntityId.EMPTY_ID) {
+                if (currentMinuend == null) {
+                    this.currentMinuend = currentMinuend = checkNextId(minuend);
+                }
+                if (currentSubtrahend == null) {
+                    this.currentSubtrahend = currentSubtrahend = checkNextId(subtrahend);
+                }
+                // no more ids in minuend
+                if ((nextId = currentMinuend) == PersistentEntityId.EMPTY_ID) {
+                    break;
+                }
+                // no more ids subtrahend
+                if (currentSubtrahend == PersistentEntityId.EMPTY_ID) {
+                    currentMinuend = null;
+                    break;
+                }
+                if (currentMinuend != currentSubtrahend && (currentMinuend == null || currentSubtrahend == null)) {
+                    break;
+                }
+                if (currentMinuend == currentSubtrahend) {
+                    currentMinuend = currentSubtrahend = null;
+                    continue;
+                }
+                final int cmp = currentMinuend.compareTo(currentSubtrahend);
+                if (cmp > 0) {
+                    currentMinuend = null;
+                    break;
+                }
+                currentSubtrahend = null;
+                if (cmp == 0) {
+                    currentMinuend = null;
+                }
+            }
+            this.currentMinuend = currentMinuend;
+            this.currentSubtrahend = currentSubtrahend;
+            return this.currentMinuend != PersistentEntityId.EMPTY_ID;
         }
+    }
 
-        @Nullable
-        private static EntityId nextId(final EntityIterator it) {
-            return it.hasNext() ? it.nextId() : PersistentEntityId.EMPTY_ID;
-        }
+    @Nullable
+    private static EntityId checkNextId(final EntityIterator it) {
+        return it.hasNext() ? it.nextId() : PersistentEntityId.EMPTY_ID;
     }
 
     private static final class UnsortedIterator extends NonDisposableEntityIterator {
