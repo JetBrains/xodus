e89f0c1f35d4b16414c29c35e6c5af73c38055ef
Andrey Lomakin <andrey.lomakin@jetbrains.com>
2022-09-01 17:44:38 +0200

    Ensure that the same data generated for each benchmark.

--- benchmarks/src/jmh/java/jetbrains/exodus/benchmark/TokyoCabinetBenchmark.java
+++ benchmarks/src/jmh/java/jetbrains/exodus/benchmark/TokyoCabinetBenchmark.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010 - 2022 JetBrains s.r.o.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * https://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,7 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Random;
 
 public class TokyoCabinetBenchmark {
 
@@ -85,6 +86,6 @@ public class TokyoCabinetBenchmark {
     }
 
     public static <T> void shuffleKeys(final T[] keys) {
-        Collections.shuffle(Arrays.asList(keys));
+        Collections.shuffle(Arrays.asList(keys), new Random(77634963005211L));
     }
 }
