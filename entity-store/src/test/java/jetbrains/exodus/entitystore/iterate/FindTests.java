9b6d234f616a2f6618d3b2ff4ada12160894313c
laa <andrey.lomakin@jetbrains.com>
2022-12-13 11:01:13 +0100

    Issue #XD-902 fixed

--- entity-store/src/test/java/jetbrains/exodus/entitystore/iterate/FindTests.java
+++ entity-store/src/test/java/jetbrains/exodus/entitystore/iterate/FindTests.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * https://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -93,6 +93,40 @@ public class FindTests extends EntityStoreTestBase {
         }
         Assert.assertEquals(10, count);
     }
+
+    @TestFor(issue = "XD-902")
+    public void testFindEmptyCached() throws Exception {
+        final StoreTransaction txn = getStoreTransactionSafe();
+        final Entity entity = txn.newEntity("Issue");
+        entity.setProperty("unlike", "...");
+        txn.flush();
+
+        EntityIterable issues = txn.findContaining("Issue", "unlike", "like",
+                false);
+
+        int count = 0;
+        for (final Entity ignored : issues) {
+            count++;
+        }
+
+        Assert.assertEquals(0, count);
+        //sleep to ensure that property iterable was cached
+        Thread.sleep(50);
+        txn.flush();
+
+        entity.delete();
+        txn.flush();
+
+        issues = txn.findContaining("Issue", "unlike", "like",
+                false);
+        count = 0;
+        for (final Entity ignored : issues) {
+            count++;
+        }
+
+        Assert.assertEquals(0, count);
+    }
+
 
     @TestFor(issue = "XD-824")
     public void testFindContainingIgnoreCase() {
