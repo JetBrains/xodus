2fbf7d63811aeb1fc9b4d6bdbb749aec2d8ce87a
laa <andrey.lomakin@jetbrains.com>
2022-12-16 13:38:55 +0100

    Backup logging was extended.

--- entity-store/src/main/java/jetbrains/exodus/entitystore/PersistentEntityStoreBackupStrategy.java
+++ entity-store/src/main/java/jetbrains/exodus/entitystore/PersistentEntityStoreBackupStrategy.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010 - 2022 JetBrains s.r.o.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * https://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -48,6 +48,8 @@ public class PersistentEntityStoreBackupStrategy extends BackupStrategy {
                     }
                     final File f = file.getFile();
                     if (f != null && fsBlobVault.getBlobHandleByFile(f) > lastUsedHandle) {
+                        System.out.println("VaultBackup : file " + f + " handle " + fsBlobVault.getBlobHandleByFile(f)
+                                + " was rejected.");
                         return -1L;
                     }
                     return super.acceptFile(file);
@@ -79,6 +81,7 @@ public class PersistentEntityStoreBackupStrategy extends BackupStrategy {
                 store.ensureBlobsConsistency(txn);
             }
 
+            System.out.println("Backup: last used handle - " + lastUsedHandle);
         } finally {
             txn.abort();
         }
