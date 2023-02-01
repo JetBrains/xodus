c4d9c73bba0bbf2e06fa72c1643fb2ffe3bf5064
Lesslyrics <alina_boshenko@mail.ru>
2023-02-01 20:30:45 +0100

    added cas and other improvments, still WIP

--- environment/src/main/java/jetbrains/exodus/log/NewConceptLog.java
+++ environment/src/main/java/jetbrains/exodus/log/NewConceptLog.java
@@ -1,11 +1,9 @@
 package jetbrains.exodus.log;
+import jetbrains.exodus.ExodusException;
 import org.jctools.maps.NonBlockingHashMapLong;
 
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.*;
 import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 
@@ -13,7 +11,7 @@ class OperationLogRecord {
     long key;
     long value;
     long transactionId;
-    String operation; // todo shouldn't be strings
+    String operation; // shouldn't be strings, but leave like this for now
 
     OperationLogRecord(long key, long value, long transactionId, String operation) {
         this.key = key;
@@ -24,10 +22,10 @@ class OperationLogRecord {
 }
 
 class MVCCRecord {
-    long maxTransactionId;
+    AtomicLong maxTransactionId;
     SortedMap<Long, Long> linksToOperations; // txId + link to record in OL
 
-    MVCCRecord(long maxTransactionId, SortedMap<Long, Long> linksToOperations) {
+    MVCCRecord(AtomicLong maxTransactionId, SortedMap<Long, Long> linksToOperations) {
         this.maxTransactionId = maxTransactionId;
         this.linksToOperations = linksToOperations;
     }
@@ -40,66 +38,71 @@ class MVCCDataStructure {
 
     private static final AtomicLong address = new AtomicLong();
 
+    private static void compareWithCurrentAndSet(MVCCRecord mvccRecord, long currentTransactionId) {
+        while(true) {
+            long value = mvccRecord.maxTransactionId.get();
+            // prohibit any write transaction to spoil situation here
+            if (value < currentTransactionId
+                    && mvccRecord.maxTransactionId.compareAndSet(value, currentTransactionId)) {
+                break;
+            }
+        }
+    }
 
-    // todo: separate for with duplicates and without, for now without
-
+    // should be separate for with duplicates and without, for now we do without only
     public static long readLogRecord(long currentTransactionId, long key) {
-        var keyHashCode = key.hashCode();
+        var keyHashCode = Long.hashCode(key);
+
         // todo: put block not to add transactions less than ours
         // replace to compute (with lock) - get mvcc record and update (atomic long) maxtransactionID, compareAndSet
-        // compute:
-        MVCCRecord mvccRecord = hashMap.get(keyHashCode); //todo replace to compute (same as in write, for GC)
+        // todo: seems we don't need a custom compute here as autoboxing is not an issue here
+        MVCCRecord mvccRecord = hashMap.compute(keyHashCode, (k, v) -> {
+            if (hashMap.get(keyHashCode) == null){
+                hashMap.put(keyHashCode, new MVCCRecord(0, new SortedMap<Long, Long>()));
+            }
+            return hashMap.get(keyHashCode);
+        });
+
         // after compute execution we have mvccRecord in any case (instead of mvccRecord == null)
         // if the id of write transaction is less than ours, we prohibit to insert here
         // this inserted mvccRecord will be removed by GC
-        // optimization: if during the execution of the compute we don't have any write transaction with id < ours, we don't need to insert
+        // optimization: if during the execution of the compute we don't have any write transaction with id < ours,
+        // we don't need to insert
         // we need to make sure that we don't have such write transaction (or we have it)
-//        if (mvccRecord == null) { // we don;t need it
-//            return -1;
-//        } else {
-            if (mvccRecord.maxTransactionId < currentTransactionId) { // todo convert to cas (gonna be a cycle here)
-                mvccRecord.maxTransactionId = currentTransactionId; // here we update and prohibit any write transaction to spol situation here
-            }
-//        }
+
+        compareWithCurrentAndSet(mvccRecord, currentTransactionId);
 
         // find maximum id smaller than the id of the current transaction
         SortedMap<Long, Long> maxTxId;
-        // todo: do binary search instead of cycle
         maxTxId = mvccRecord.linksToOperations.headMap(currentTransactionId);
+        SortedMap<Long, Long> reverseMaxTxId =  maxTxId.reverse(); // from max to min
+//        TreeMap<Long, Long> reverseTreeMap = new TreeMap(maxTxId).descendingMap();
 
-        SortedMap<Long, Long> maxTxId2 =  maxTxId.reverse(); // from max to min
-        // todo iterate operations and get log record, compare keys until they are matching, instead of
-        //  OperationLogRecord logRecord = operationLog.get(mvccRecord.linksToOperations.get(maxTxId));
-
-
-        // todo: key: byteArray (for later)
-//        OperationLogRecord logRecord = operationLog.get(mvccRecord.linksToOperations.get(maxTxId));
-//        if (logRecord.key.equals(key)){
-//            return logRecord.value;
-//        } else {
-//            return -1;
-//        }
-
+        // iterate operations and get log record, compare keys until they are matching
+        for (Long operationKey : operationLog.keySet()) {
+            // todo: do binary search instead of cycle, use reverseMaxTxId and thread-safe comparison
+            if (operationLog.get(operationKey).key == key){
+                return operationLog.get(operationKey).value;
+            }
+        }
+        return -1 ;// todo: what to do if no match - put in cycle
     }
 
-    public static void write(long transactionId, long key, long value) {
-        long keyHashCode = key.hashCode();
+    public static void write(long transactionId, long key, long value, String operation) {
+        long keyHashCode = Long.hashCode(key);
         MVCCRecord mvccRecord = hashMap.get(keyHashCode);
         if (mvccRecord == null) {
-            mvccRecord = new MVCCRecord(transactionId, new ConcurrentHashMap<>());
+            mvccRecord = new MVCCRecord(transactionId, new SortedMap());
             hashMap.put(keyHashCode, mvccRecord);
         } // todo replace to compute
 
-        if (transactionId < mvccRecord.maxTransactionId) {
-            // do rollback (XodusException)
+        if (transactionId < mvccRecord.maxTransactionId.get()) {
+            throw new ExodusException(); // rollback
         }
 
-        ArrayList<String> operations = new ArrayList<>();
-        var addr = address.getAndIncrement();
-
-        operationLog.put(addr, new OperationLogRecord(key, value, transactionId, operations));
-
-        mvccRecord.linksToOperations.put(transactionId, addr);
+        var recordAddress = address.getAndIncrement();
+        operationLog.put(recordAddress, new OperationLogRecord(key, value, transactionId, operation));
+        mvccRecord.linksToOperations.put(transactionId, recordAddress);
     }
 }
 
