133f123d768c39de773bee9d05a5544f7a58f268
Lesslyrics <alina_boshenko@mail.ru>
2023-02-08 16:31:25 +0100

    minor refactoring

--- environment/src/main/java/jetbrains/exodus/log/MVCCComponent.java
+++ environment/src/main/java/jetbrains/exodus/log/MVCCComponent.java
@@ -12,28 +12,6 @@ import java.util.function.Function;
 
 import static jetbrains.exodus.log.BufferedDataWriter.*;
 
-enum OperationReferenceState {
-    IN_PROGRESS,
-    ABORTED,
-    COMPLETED
-}
-
-class OperationReferenceEntry {
-    final long operationAddress; // not an array as we have multiple entries of OperationsLinksEntry in queue with same txId
-    volatile OperationReferenceState state = OperationReferenceState.IN_PROGRESS;
-    final long txId;
-
-
-    OperationReferenceEntry(long linkToOperation, long txId) {
-        this.operationAddress = linkToOperation;
-        this.txId = txId;
-    }
-
-    public long getTxId() {
-        return this.txId;
-    }
-}
-
 class MVCCRecord {
     final AtomicLong maxTransactionId;
     final MpmcUnboundedXaddArrayQueue<OperationReferenceEntry> linksToOperationsQueue; // array of links to record in OL
---
+++ environment/src/main/java/jetbrains/exodus/log/OperationReference.java
@@ -0,0 +1,23 @@
+package jetbrains.exodus.log;
+
+enum OperationReferenceState {
+    IN_PROGRESS,
+    ABORTED,
+    COMPLETED
+}
+
+class OperationReferenceEntry {
+    final long operationAddress; // not an array as we have multiple entries of OperationsLinksEntry in queue with same txId
+    volatile OperationReferenceState state = OperationReferenceState.IN_PROGRESS;
+    final long txId;
+
+
+    OperationReferenceEntry(long linkToOperation, long txId) {
+        this.operationAddress = linkToOperation;
+        this.txId = txId;
+    }
+
+    public long getTxId() {
+        return this.txId;
+    }
+}
