3bc2e4e1378bb8c18ddb54ad18ee35a19eadb9e6
laa <andrey.lomakin@jetbrains.com>
2023-04-14 22:33:34 +0200

    Not needed dependencies have been removed.

--- entity-store/build.gradle.kts
+++ entity-store/build.gradle.kts
@@ -4,6 +4,5 @@ dependencies {
     implementation(project(":xodus-environment"))
     implementation(project(":xodus-compress"))
     implementation(libs.commons.io)
-    testImplementation("io.github.classgraph:classgraph:4.8.90")
     testImplementation(project(":xodus-utils", "testArtifacts"))
 }
