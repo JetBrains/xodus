b695dca596c5706f7e349fb71de6814b0271caff
laa <andrey.lomakin@jetbrains.com>
2023-04-14 22:38:18 +0200

    Revert "Not needed dependencies have been removed."
    This reverts commit 3bc2e4e1378bb8c18ddb54ad18ee35a19eadb9e6.

--- entity-store/build.gradle.kts
+++ entity-store/build.gradle.kts
@@ -4,5 +4,6 @@ dependencies {
     implementation(project(":xodus-environment"))
     implementation(project(":xodus-compress"))
     implementation(libs.commons.io)
+    testImplementation("io.github.classgraph:classgraph:4.8.90")
     testImplementation(project(":xodus-utils", "testArtifacts"))
 }
