c2dcf25e8660ca82ac3c9005cace3937039590d2
laa <andrey.lomakin@jetbrains.com>
2023-06-25 08:16:01 +0200

    Pattern of print assembly has been changed.

--- disk-ann/build.gradle.kts
+++ disk-ann/build.gradle.kts
@@ -42,7 +42,10 @@ tasks {
             "-Djava.awt.headless=true",
             "-XX:+PrintCompilation",
             "-XX:+PrintInlining",
-            "-XX:CompileCommand=print,\"jetbrains.exodus.diskann.DiskANN::computeL2Distance\"",
+            "-XX:CompileCommand=print,jetbrains.exodus.diskann/DiskANN.computeL2Distance",
+            "-XX:CompileCommand=log,jetbrains.exodus.diskann/*.*",
+            "-XX:CompileCommand=log,jetbrains.exodus.diskann/*.*",
+            "-XX:CompileCommand=log,jdk.incubator.vector/*.*",
             "--enable-preview"
         )
         systemProperties = mapOf(
