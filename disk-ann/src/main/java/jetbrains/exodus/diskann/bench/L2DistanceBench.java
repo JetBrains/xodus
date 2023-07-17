b2a6bc8fea477ff84b88aeeab42905beab1ff3cc
laa <andrey.lomakin@jetbrains.com>
2023-07-17 18:24:13 +0200

    Decrease delay of perf in JMH L2 bench.

--- disk-ann/src/main/java/jetbrains/exodus/diskann/bench/L2DistanceBench.java
+++ disk-ann/src/main/java/jetbrains/exodus/diskann/bench/L2DistanceBench.java
@@ -121,7 +121,8 @@ public class L2DistanceBench {
 
     public static void main(String[] args) throws Exception {
         Options opt = new OptionsBuilder()
-                .include(L2DistanceBench.class.getSimpleName()).addProfiler(LinuxPerfProfiler.class, "events=fp_ret_sse_avx_ops.all")
+                .include(L2DistanceBench.class.getSimpleName()).addProfiler(LinuxPerfProfiler.class,
+                        "events=fp_ret_sse_avx_ops.all;delay=1000")
                 .build();
         new Runner(opt).run();
     }
