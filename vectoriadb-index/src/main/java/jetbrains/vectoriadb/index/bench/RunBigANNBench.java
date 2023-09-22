8fab49c1cf149dd2f8b1942829ea367c79967359
laa <andrey.lomakin@jetbrains.com>
2023-09-22 11:21:29 +0200

    Back to 110GB cache.

--- vectoriadb-index/src/main/java/jetbrains/vectoriadb/index/bench/RunBigANNBench.java
+++ vectoriadb-index/src/main/java/jetbrains/vectoriadb/index/bench/RunBigANNBench.java
@@ -89,7 +89,7 @@ public class RunBigANNBench {
 
         System.out.println("Loading BigANN index...");
         try (var indexReader = new IndexReader(PrepareBigANNBench.INDEX_NAME, PrepareBigANNBench.VECTOR_DIMENSIONS,
-                bigAnnDbDir, 4L * 1024 * 1024 * 1024, Distance.L2)) {
+                bigAnnDbDir, 110L * 1024 * 1024 * 1024, Distance.L2)) {
 
             System.out.println("Running BigANN bench...");
             var result = new int[recallCount];
