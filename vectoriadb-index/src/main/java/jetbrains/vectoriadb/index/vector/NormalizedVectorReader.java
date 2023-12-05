06f91d23e0b200ae9d2a4ccb7fb2fcf368e3c6a0
Kirill Vasilenko <kirill.vasilenko@jetbrains.com>
2023-12-05 17:10:20 +0100

    add NormalizedVectorReader, the class that wraps a VectorReader and returns normalized vectors from the read()

---
+++ vectoriadb-index/src/main/java/jetbrains/vectoriadb/index/vector/NormalizedVectorReader.java
@@ -0,0 +1,66 @@
+package jetbrains.vectoriadb.index.vector;
+
+import jetbrains.vectoriadb.index.ProgressTracker;
+import jetbrains.vectoriadb.index.VectorReader;
+import jetbrains.vectoriadb.index.ParallelBuddy;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+
+public final class NormalizedVectorReader implements VectorReader {
+
+    private final VectorReader source;
+
+    private final Arena arena;
+    private MemorySegment vectorNorms;
+
+    public NormalizedVectorReader(VectorReader source) {
+        arena = Arena.ofShared();
+        this.source = source;
+    }
+
+    @Override
+    public int size() {
+        return source.size();
+    }
+
+    @Override
+    public int dimensions() {
+        return source.dimensions();
+    }
+
+    public void precalculateOriginalVectorNorms(@NotNull ParallelBuddy pBuddy, @NotNull ProgressTracker progressTracker) {
+        vectorNorms = arena.allocate((long) source.size() * Float.BYTES, ValueLayout.JAVA_FLOAT.byteAlignment());
+        pBuddy.execute(
+                "Original vector norms pre-calculation",
+                size(),
+                progressTracker,
+                (_, vectorIdx) -> {
+                    var vector = source.read(vectorIdx);
+                    var norm = VectorOperations.calculateL2Norm(vector, dimensions());
+                    vectorNorms.setAtIndex(ValueLayout.JAVA_FLOAT, vectorIdx, norm);
+                }
+        );
+    }
+
+    public float getOriginalVectorNorm(int index) {
+        return vectorNorms.getAtIndex(ValueLayout.JAVA_FLOAT, index);
+    }
+
+    @Override
+    public MemorySegment read(int index) {
+        var vector = source.read(index);
+        var norm = vectorNorms.getAtIndex(ValueLayout.JAVA_FLOAT, index);
+        var result = MemorySegment.ofArray(new float[dimensions()]);
+
+        VectorOperations.normalizeL2(vector, norm, result, dimensions());
+        return result;
+    }
+
+    @Override
+    public void close() {
+        arena.close();
+    }
+}
---
+++ vectoriadb-index/src/test/java/jetbrains/vectoriadb/index/vector/NormalizedVectorReaderTest.kt
@@ -0,0 +1,36 @@
+package jetbrains.vectoriadb.index.vector
+
+import jetbrains.vectoriadb.index.NoOpProgressTracker
+import jetbrains.vectoriadb.index.ParallelBuddy
+import jetbrains.vectoriadb.index.vector.VectorOperations.PRECISION
+import org.junit.Assert
+import org.junit.Test
+
+class NormalizedVectorReaderTest {
+
+    @Test
+    fun `read normalized vectors`() {
+        val progressTracker = NoOpProgressTracker()
+        val pBuddy = ParallelBuddy(1, "test")
+
+        val count = 10
+        val dimensions = 30
+        val segment = createRandomFloatVectorSegment(count, dimensions)
+        val segmentReader = FloatVectorSegmentReader(segment)
+        val normReader = NormalizedVectorReader(segmentReader)
+
+        normReader.precalculateOriginalVectorNorms(pBuddy, progressTracker)
+
+        repeat(count) { vectorIdx ->
+            val precalcNorm = normReader.getOriginalVectorNorm(vectorIdx)
+            val norm = VectorOperations.calculateL2Norm(segment.get(vectorIdx), dimensions)
+            Assert.assertEquals(norm, precalcNorm, PRECISION)
+        }
+
+        repeat(count) { vectorIdx ->
+            val vector = normReader.read(vectorIdx)
+            val norm = VectorOperations.calculateL2Norm(vector, dimensions)
+            Assert.assertEquals(1f, norm, PRECISION)
+        }
+    }
+}
