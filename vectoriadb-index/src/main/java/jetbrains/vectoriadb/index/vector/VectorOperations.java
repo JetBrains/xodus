5893f49741c2dfe6a2bd28c053dfb1874498d963
Kirill Vasilenko <kirill.vasilenko@jetbrains.com>
2023-12-05 13:06:49 +0100

    add VectorOperations and tests for them

---
+++ vectoriadb-index/src/main/java/jetbrains/vectoriadb/index/vector/VectorOperations.java
@@ -0,0 +1,129 @@
+package jetbrains.vectoriadb.index.vector;
+
+import jdk.incubator.vector.FloatVector;
+import jdk.incubator.vector.VectorMask;
+import jdk.incubator.vector.VectorOperators;
+import jdk.incubator.vector.VectorSpecies;
+
+import java.lang.foreign.MemorySegment;
+import java.nio.ByteOrder;
+
+import static java.lang.Math.abs;
+
+public final class VectorOperations {
+
+    public static final float PRECISION = (float) 1e-5;
+
+    /**
+     * vector1 + vector2 -> result
+     * */
+    public static void add(MemorySegment v1, long idx1, MemorySegment v2, long idx2, MemorySegment result, long resultIdx, int size) {
+        var species = FloatVector.SPECIES_PREFERRED;
+        var step = species.length();
+        for (int i = 0; i < size; i += step) {
+            var mask = species.indexInRange(i, size);
+            var V1 = toVector(v1, idx1 + i, species, mask);
+            var V2 = toVector(v2, idx2 + i, species, mask);
+
+            var R = V1.add(V2);
+            intoResult(R, result, resultIdx + i, mask);
+        }
+    }
+
+    /**
+     * vector1 / scalar -> result
+     * */
+    public static void div(MemorySegment v1, long idx1, float scalar, MemorySegment result, long resultIdx, int size) {
+        var species = FloatVector.SPECIES_PREFERRED;
+        var step = species.length();
+        for (int i = 0; i < size; i += step) {
+            var mask = species.indexInRange(i, size);
+            var V1 = toVector(v1, idx1 + i, species, mask);
+
+            var R = V1.div(scalar);
+            intoResult(R, result, resultIdx + i, mask);
+        }
+    }
+
+    public static float innerProduct(MemorySegment v1, long idx1, MemorySegment v2, long idx2, int size) {
+        var species = FloatVector.SPECIES_PREFERRED;
+        var step = species.length();
+        var result = 0f;
+        for (int i = 0; i < size; i += step) {
+            var mask = species.indexInRange(i, size);
+            var V1 = toVector(v1, idx1 + i, species, mask);
+            var V2 = toVector(v2, idx2 + i, species, mask);
+
+            var R = V1.mul(V2);
+            result += R.reduceLanes(VectorOperators.ADD, mask);
+        }
+        return result;
+    }
+
+    public static float calculateL2Norm(float[] vector) {
+        var v = MemorySegment.ofArray(vector);
+        return (float) Math.sqrt(innerProduct(v, 0, v, 0, vector.length));
+    }
+
+    public static float calculateL2Norm(MemorySegment vector, int size) {
+        return (float) Math.sqrt(innerProduct(vector, 0, vector, 0, size));
+    }
+
+    /**
+     * Normalizes the vector by L2 norm, writes result to the result
+    * */
+    public static void normalizeL2(float[] vector, float[] result) {
+        var vSegment = MemorySegment.ofArray(vector);
+        normalizeL2(vSegment, calculateL2Norm(vSegment, vector.length), MemorySegment.ofArray(result), vector.length);
+    }
+
+    public static void normalizeL2(MemorySegment vector, float vectorNorm, MemorySegment result, int size) {
+        if (abs(vectorNorm - 1) > PRECISION) {
+            var species = FloatVector.SPECIES_PREFERRED;
+            var step = species.length();
+            for (int i = 0; i < size; i += step) {
+                var mask = species.indexInRange(i, size);
+                var V1 = toVector(vector, i, species, mask);
+
+                var R = V1.div(vectorNorm);
+                intoResult(R, result, i, mask);
+            }
+        } else {
+            result.copyFrom(vector);
+        }
+
+        assert abs(calculateL2Norm(result, size) - 1) < PRECISION;
+    }
+
+    private static FloatVector toVector(MemorySegment v, long valueIdx, VectorSpecies<Float> species, VectorMask<Float> mask) {
+        /*
+         * Vector API supports only heap-based segments with byte[] arrays behind.
+         * If we use heap-based segments with float[] arrays behind, we will get a strange exception at runtime.
+         * Most probably it is a temporary problem and some day they will fix it.
+         * But for now, we have to hack this case around.
+         * */
+        FloatVector V;
+        if (v.heapBase().isEmpty()) {
+            V = FloatVector.fromMemorySegment(species, v, valueIdx * Float.BYTES, ByteOrder.nativeOrder(), mask);
+        } else {
+            var arr = (float[]) v.heapBase().get();
+            V = FloatVector.fromArray(species, arr, (int) valueIdx, mask);
+        }
+        return V;
+    }
+
+    private static void intoResult(FloatVector R, MemorySegment result, long valueIdx, VectorMask<Float> mask) {
+        /*
+         * Vector API supports only heap-based segments with byte[] arrays behind.
+         * If we use heap-based segments with float[] arrays behind, we will get a strange exception at runtime.
+         * Most probably it is a temporary problem and some day they will fix it.
+         * But for now, we have to hack this case around.
+         * */
+        if (result.heapBase().isEmpty()) {
+            R.intoMemorySegment(result, valueIdx * Float.BYTES, ByteOrder.nativeOrder(), mask);
+        } else {
+            var arr = (float[]) result.heapBase().get();
+            R.intoArray(arr, (int) valueIdx, mask);
+        }
+    }
+}
---
+++ vectoriadb-index/src/test/java/jetbrains/vectoriadb/index/vector/VectorOperationsTest.kt
@@ -0,0 +1,135 @@
+package jetbrains.vectoriadb.index.vector
+
+import jetbrains.vectoriadb.index.vector.VectorOperations.PRECISION
+import org.junit.Assert
+import org.junit.Test
+import java.lang.foreign.Arena
+import java.lang.foreign.MemorySegment
+import java.lang.foreign.ValueLayout
+import kotlin.math.abs
+
+class VectorOperationsTest {
+
+    @Test
+    fun add() = Arena.ofConfined().use { arena ->
+        val v1 = arena.allocateFloat(300)
+        v1.fill(1f, 0, 100)
+        v1.fill(2f, 100, 100)
+        v1.fill(3f, 200, 100)
+
+        val v2 = arena.allocateFloat(300)
+        v2.fill(10f, 0, 100)
+        v2.fill(20f, 100, 100)
+        v2.fill(30f, 200, 100)
+
+        VectorOperations.add(v1, 0, v2, 0, v1, 0, 100)
+        VectorOperations.add(v1, 100, v2, 100, v1, 100, 100)
+        VectorOperations.add(v1, 200, v2, 200, v1, 200, 100)
+
+        v1.check(11f, 0, 100)
+        v1.check(22f, 100, 100)
+        v1.check(33f, 200, 100)
+        v2.check(10f, 0, 100)
+        v2.check(20f, 100, 100)
+        v2.check(30f, 200, 100)
+    }
+
+    @Test
+    fun `add skewed vectors`() = Arena.ofConfined().use { arena ->
+        val v1 = arena.allocateFloat(310)
+        v1.fill(1f, 10, 100)
+        v1.fill(2f, 110, 100)
+        v1.fill(3f, 210, 100)
+
+        val v2 = arena.allocateFloat(350)
+        v2.fill(10f, 50, 100)
+        v2.fill(20f, 150, 100)
+        v2.fill(30f, 250, 100)
+
+        VectorOperations.add(v1, 10, v2, 50, v1, 10, 100)
+        VectorOperations.add(v1, 110, v2, 150, v1, 110, 100)
+        VectorOperations.add(v1, 210, v2, 250, v1, 210, 100)
+
+        v1.check(11f, 10, 100)
+        v1.check(22f, 110, 100)
+        v1.check(33f, 210, 100)
+
+        v2.check(10f, 50, 100)
+        v2.check(20f, 150, 100)
+        v2.check(30f, 250, 100)
+    }
+
+    @Test
+    fun `div by scalar`() = Arena.ofConfined().use { arena ->
+        val v1 = arena.allocateFloat(300)
+        v1.fill(100f, 0, 100)
+        v1.fill(90f, 100, 100)
+        v1.fill(50f, 200, 100)
+
+        VectorOperations.div(v1, 0, 2f, v1, 0, 100)
+        VectorOperations.div(v1, 100, 3f, v1, 100, 100)
+        VectorOperations.div(v1, 200, 5f, v1, 200, 100)
+
+        v1.check(50f, 0, 100)
+        v1.check(30f, 100, 100)
+        v1.check(10f, 200, 100)
+    }
+
+    @Test
+    fun innerProduct() {
+        val v1 = MemorySegment.ofArray(floatArrayOf(100f, 1f, 2f, 3f, 100f))
+        val v2 = MemorySegment.ofArray(floatArrayOf(100f, 100f, 4f, 5f, 6f, 100f))
+
+        val result = VectorOperations.innerProduct(v1, 1, v2, 2, 3)
+        Assert.assertEquals(32f, result, PRECISION)
+    }
+
+    @Test
+    fun `calculate L2 norm`() {
+        val v1Array = floatArrayOf(1f, 3f, 1f, 3f, 1f, 2f)
+        val v1Segment = MemorySegment.ofArray(v1Array)
+
+        val arrayNorm = VectorOperations.calculateL2Norm(v1Array)
+        val segmentNorm = VectorOperations.calculateL2Norm(v1Segment, v1Array.size)
+
+        Assert.assertEquals(5f, segmentNorm, PRECISION)
+        Assert.assertEquals(arrayNorm, segmentNorm, PRECISION)
+    }
+
+    @Test
+    fun `normalize L2`() {
+        val v1Array = floatArrayOf(1f, 2f, 3f, 4f, 5f)
+        val v1Segment = MemorySegment.ofArray(v1Array)
+        val resultArray = FloatArray(v1Array.size)
+        val resultSegment = MemorySegment.ofArray(FloatArray(v1Array.size))
+
+        VectorOperations.normalizeL2(v1Array, resultArray)
+        VectorOperations.normalizeL2(v1Segment, VectorOperations.calculateL2Norm(v1Array), resultSegment, v1Array.size)
+
+        Assert.assertEquals(1f, VectorOperations.calculateL2Norm(resultArray), PRECISION)
+        Assert.assertEquals(VectorOperations.calculateL2Norm(resultSegment, v1Array.size), VectorOperations.calculateL2Norm(resultArray), PRECISION)
+
+        Assert.assertNotEquals(1f, VectorOperations.calculateL2Norm(v1Array), PRECISION)
+        Assert.assertEquals(VectorOperations.calculateL2Norm(v1Segment, v1Array.size), VectorOperations.calculateL2Norm(v1Array), PRECISION)
+    }
+
+    private fun MemorySegment.check(value: Float, fromIdx: Int, count: Int) {
+        var idx = fromIdx
+        repeat(count) {
+            assert(abs(getAtIndex(ValueLayout.JAVA_FLOAT, idx.toLong()) - value) < PRECISION)
+            idx++
+        }
+    }
+
+    private fun MemorySegment.fill(value: Float, fromIdx: Int, count: Int) {
+        var idx = fromIdx
+        repeat(count) {
+            setAtIndex(ValueLayout.JAVA_FLOAT, idx.toLong(), value)
+            idx++
+        }
+    }
+
+    private fun Arena.allocateFloat(count: Int): MemorySegment {
+        return allocate(count.toLong() * Float.SIZE_BYTES, ValueLayout.JAVA_FLOAT.byteAlignment())
+    }
+}
