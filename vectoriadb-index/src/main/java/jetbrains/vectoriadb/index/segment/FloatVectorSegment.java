6e1377bb7bb70454050aba4f2b3f24e0b6ec45c5
Kirill Vasilenko <kirill.vasilenko@jetbrains.com>
2023-12-05 13:26:14 +0100

    add FloatVectorSegment, the class that helps to work with vectors allocated as MemorySegments

---
+++ vectoriadb-index/src/main/java/jetbrains/vectoriadb/index/segment/FloatVectorSegment.java
@@ -0,0 +1,104 @@
+package jetbrains.vectoriadb.index.segment;
+
+import jetbrains.vectoriadb.index.vector.VectorOperations;
+
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+
+public final class FloatVectorSegment {
+    private static final ValueLayout.OfFloat LAYOUT = ValueLayout.JAVA_FLOAT;
+    private static final int BYTES = Float.BYTES;
+
+    private final int dimensions;
+    private final int count;
+    private final MemorySegment vectors;
+
+    public FloatVectorSegment(int count, int dimensions, MemorySegment vectors) {
+        this.dimensions = dimensions;
+        this.count = count;
+        this.vectors = vectors;
+    }
+
+    public int getDimensions() {
+        return dimensions;
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public void add(int idx1, FloatVectorSegment v2, int idx2) {
+        add(idx1, v2.vectors, idx2);
+    }
+
+    public void add(int idx1, MemorySegment v2, int idx2) {
+        VectorOperations.add(vectors, (long) idx1 * dimensions, v2, (long) idx2 * dimensions, vectors, (long) idx1 * dimensions, dimensions);
+    }
+
+    public void div(int idx, float scalar) {
+        VectorOperations.div(vectors, (long) idx * dimensions, scalar, vectors, (long) idx * dimensions, dimensions);
+    }
+
+    public float get(int vectorIdx, int dimension) {
+        return vectors.getAtIndex(LAYOUT,(long) vectorIdx * dimensions + dimension);
+    }
+
+    public void set(int vectorIdx, int dimension, float value) {
+        vectors.setAtIndex(LAYOUT, (long) vectorIdx * dimensions + dimension, value);
+    }
+
+    public void set(int vectorIdx, MemorySegment vector) {
+        MemorySegment.copy(vector, LAYOUT, 0, vectors, vectorIdx, dimensions);
+    }
+
+    public MemorySegment get(int vectorIdx) {
+        return vectors.asSlice((long) vectorIdx * dimensions, LAYOUT);
+    }
+
+    public void fill(byte value) {
+        vectors.fill(value);
+    }
+
+    public FloatVectorSegment copy() {
+        return new FloatVectorSegment(count, dimensions, MemorySegment.ofArray(vectors.toArray(ValueLayout.JAVA_FLOAT)));
+    }
+
+    public boolean equals(int idx1, FloatVectorSegment v2, int idx2) {
+        for (int i = 0; i < dimensions; i++) {
+            if (Math.abs(this.get(idx1, i) - v2.get(idx2, i)) > VectorOperations.PRECISION) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(FloatVectorSegment v2) {
+        if (count != v2.count) return false;
+        if (dimensions != v2.dimensions) return false;
+        for (int i = 0; i < count; i++) {
+            if (!equals(i, v2, i)) return false;
+        }
+        return true;
+    }
+
+    public static FloatVectorSegment makeNativeSegment(Arena arena, int count, int dimensions) {
+        var segment = arena.allocate((long) count * dimensions * BYTES, LAYOUT.byteAlignment());
+        return new FloatVectorSegment(count, dimensions, segment);
+    }
+
+    public static FloatVectorSegment makeArraySegment(int count, int dimensions) {
+        var array = new float[count * dimensions];
+        return new FloatVectorSegment(count, dimensions, MemorySegment.ofArray(array));
+    }
+
+    public static FloatVectorSegment[] makeNativeSegments(Arena arena, int segmentCount, int vectorsPerSegment, int dimensions) {
+        var segment = arena.allocate((long) segmentCount * vectorsPerSegment * dimensions * BYTES, LAYOUT.byteAlignment());
+
+        var result = new FloatVectorSegment[segmentCount];
+        for (int i = 0; i < segmentCount; i++) {
+            result[i] = new FloatVectorSegment(vectorsPerSegment, dimensions, segment.asSlice((long) i * vectorsPerSegment * dimensions * Float.BYTES, (long) vectorsPerSegment * dimensions * Float.BYTES));
+        }
+        return result;
+    }
+}
---
+++ vectoriadb-index/src/test/java/jetbrains/vectoriadb/index/segment/FloatVectorSegmentTest.kt
@@ -0,0 +1,189 @@
+package jetbrains.vectoriadb.index.segment
+
+import org.junit.Test
+import java.lang.foreign.Arena
+import kotlin.random.Random
+
+class FloatVectorSegmentTest {
+
+    private val dimensions = 111
+
+    @Test
+    fun equals() {
+        val count = 5
+        val v1 = FloatVectorSegment.makeArraySegment(count, dimensions)
+        val v2 = FloatVectorSegment.makeArraySegment(count, dimensions)
+        repeat(count) { vi ->
+            repeat(dimensions) { di ->
+                val value = Random.nextDouble(1000.0).toFloat()
+                v1.set(vi, di, value)
+                v2.set(vi, di, value)
+            }
+        }
+
+        repeat(count) { vi ->
+            assert(v1.equals(vi, v2, vi))
+        }
+    }
+
+    @Test
+    fun copy() {
+        Arena.ofConfined().use { arena ->
+            val count = 5
+            val v = arena.createRandomVectorSegment(count)
+            val vCopy = v.copy()
+            assert(v.equals(vCopy))
+        }
+    }
+
+    @Test
+    fun `simplest add`() = Arena.ofConfined().use { arena ->
+        listOf(true, false).forEach { heapBasedSegments ->
+            val v1 = arena.createRandomVectorSegment(1, heapBasedSegments)
+            val v2 = arena.createRandomVectorSegment(1, heapBasedSegments)
+
+            val v2Copy = v2.copy()
+            val expectedResult = naiveSum(v1, 0, v2, 0)
+
+            v1.add(0, v2, 0)
+
+            // v2 remains the same
+            assert(v2.equals(v2Copy))
+
+            // v1 changed
+            assert(v1.equals(0, expectedResult, 0))
+        }
+    }
+
+
+    @Test
+    fun `simple add`() = Arena.ofConfined().use { arena ->
+        listOf(true, false).forEach { heapBasedSegments ->
+            val count1 = 2
+            val count2 = 2
+            val v1 = arena.createSequentialVectorSegment(count1, 1f, heapBasedSegments)
+            val v2 = arena.createSequentialVectorSegment(count2, 3f, heapBasedSegments)
+            val idx1 = 1
+            val idx2 = 1
+
+            val v1Copy = v1.copy()
+            val v2Copy = v2.copy()
+            val expectedResult = naiveSum(v1, idx1, v2, idx2)
+
+            v1.add(idx1, v2, idx2)
+
+            // v2 remains the same
+            for (i in 0 until count2) {
+                assert(v2.equals(i, v2Copy, i))
+            }
+
+            // only idx1 changed in v1
+            for (i in 0 until count1) {
+                if (i == idx1) continue
+                assert(v1.equals(i, v1Copy, i))
+            }
+            assert(v1.equals(idx1, expectedResult, 0))
+        }
+    }
+
+    @Test
+    fun add() = Arena.ofConfined().use { arena ->
+        listOf(false, true).forEach { heapBasedSegments ->
+            val count1 = 4
+            val count2 = 5
+            val v1 = arena.createRandomVectorSegment(count1, heapBasedSegments)
+            val v2 = arena.createRandomVectorSegment(count2, heapBasedSegments)
+            val idx1 = 2
+            val idx2 = 3
+
+            val v1Copy = v1.copy()
+            val v2Copy = v2.copy()
+            val expectedResult = naiveSum(v1, idx1, v2, idx2)
+
+            v1.add(idx1, v2, idx2)
+
+            // v2 remains the same
+            for (i in 0 until count2) {
+                assert(v2.equals(i, v2Copy, i))
+            }
+
+            // only idx1 changed in v1
+            for (i in 0 until count1) {
+                if (i == idx1) continue
+                assert(v1.equals(i, v1Copy, i))
+            }
+            assert(v1.equals(idx1, expectedResult, 0))
+        }
+    }
+
+    @Test
+    fun `div by scalar`() = Arena.ofConfined().use { arena ->
+        listOf(false, true).forEach { heapBasedSegments ->
+            val count1 = 4
+            val v1 = arena.createRandomVectorSegment(count1, heapBasedSegments)
+            val idx1 = 1
+            val scalar = 13f
+
+            val v1Copy = v1.copy()
+            val expectedResult = naiveDiv(v1, idx1, scalar)
+
+            v1.div(idx1, scalar)
+
+            // only idx1 changed in v1
+            for (i in 0 until count1) {
+                if (i == idx1) continue
+                assert(v1.equals(i, v1Copy, i))
+            }
+            assert(v1.equals(idx1, expectedResult, 0))
+        }
+    }
+
+    private fun naiveSum(v1: FloatVectorSegment, idx1: Int, v2: FloatVectorSegment, idx2: Int): FloatVectorSegment {
+        val result = FloatVectorSegment.makeArraySegment(1, dimensions)
+        for (i in 0 until dimensions) {
+            result.set(0, i, v1.get(idx1, i) + v2.get(idx2, i))
+        }
+        return result
+    }
+
+    @Suppress("SameParameterValue")
+    private fun naiveDiv(v1: FloatVectorSegment, idx1: Int, scalar: Float): FloatVectorSegment {
+        val result = FloatVectorSegment.makeArraySegment(1, dimensions)
+        for (i in 0 until dimensions) {
+            result.set(0, i, v1.get(idx1, i) / scalar)
+        }
+        return result
+    }
+
+    private fun Arena.createRandomVectorSegment(count: Int, heapBased: Boolean = false): FloatVectorSegment {
+        val v1 = if (heapBased) {
+            FloatVectorSegment.makeArraySegment(count, dimensions)
+        } else {
+            FloatVectorSegment.makeNativeSegment(this, count, dimensions)
+        }
+
+        for (vectorIdx in 0 until count) {
+            for (dimension in 0 until dimensions) {
+                v1.set(vectorIdx, dimension, Random.nextDouble(1000.0).toFloat())
+            }
+        }
+        return v1
+    }
+
+    private fun Arena.createSequentialVectorSegment(count: Int, valueStartFrom: Float, native: Boolean = false): FloatVectorSegment {
+        val v1 = if (native) {
+            FloatVectorSegment.makeNativeSegment(this, count, dimensions)
+        } else {
+            FloatVectorSegment.makeArraySegment(count, dimensions)
+        }
+
+        var value = valueStartFrom
+        for (vectorIdx in 0 until count) {
+            for (dimension in 0 until dimensions) {
+                v1.set(vectorIdx, dimension, value)
+            }
+            value += 1
+        }
+        return v1
+    }
+}
