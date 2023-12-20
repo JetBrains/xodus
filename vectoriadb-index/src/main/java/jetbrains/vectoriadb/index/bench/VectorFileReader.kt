72fcc9285849ee4b7ab84ec35a467bbc191d953e
Kirill Vasilenko <kirill.vasilenko@jetbrains.com>
2023-12-20 18:04:06 +0100

    add VectorFileReader, a bunch of classes that know how to read your vector files. .fvecs and .bvecs formats are supported

---
+++ vectoriadb-index/src/main/java/jetbrains/vectoriadb/index/bench/VectorFileReader.kt
@@ -0,0 +1,144 @@
+package jetbrains.vectoriadb.index.bench
+
+import jetbrains.vectoriadb.index.IndexBuilder
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.channels.FileChannel
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import kotlin.io.path.extension
+import kotlin.math.min
+
+interface VectorFileReader: AutoCloseable {
+    fun read(idx: Int): FloatArray
+    fun read(idx: Int, vector: FloatArray)
+    fun readAll(): Array<FloatArray>
+
+    fun readId(idx: Int): ByteArray {
+        val idBuffer = ByteBuffer.allocate(IndexBuilder.VECTOR_ID_SIZE)
+        idBuffer.order(ByteOrder.LITTLE_ENDIAN)
+        idBuffer.putInt(idx)
+        idBuffer.rewind()
+        return idBuffer.array()
+    }
+
+    companion object {
+        @JvmStatic
+        fun openFileReader(filePath: Path, vectorDimensions: Int, maxVectorCount: Int = Int.MAX_VALUE): VectorFileReader {
+            return when (filePath.extension) {
+                "fvecs" -> FvecsFileReader(
+                    FileChannel.open(filePath, StandardOpenOption.READ),
+                    vectorDimensions,
+                    maxVectorCount
+                )
+
+                "bvecs" -> BvecsFileReader(
+                    FileChannel.open(filePath, StandardOpenOption.READ),
+                    vectorDimensions,
+                    maxVectorCount
+                )
+                else -> throw IllegalArgumentException("The file format ${filePath.fileName} is not supported")
+            }
+        }
+
+        @JvmStatic
+        fun wrapWithFileReader(channel: FileChannel, filePath: Path, vectorDimensions: Int, maxVectorCount: Int = Int.MAX_VALUE): VectorFileReader {
+            return when (filePath.extension) {
+                "fvecs" -> FvecsFileReader(channel, vectorDimensions, maxVectorCount)
+                "bvecs" -> BvecsFileReader(channel, vectorDimensions, maxVectorCount)
+                else -> throw IllegalArgumentException("The file format ${filePath.fileName} is not supported")
+            }
+        }
+    }
+}
+
+class FvecsFileReader(
+    private val channel: FileChannel,
+    private val vectorDimensions: Int,
+    private val maxVectorCount: Int = Int.MAX_VALUE
+) : VectorFileReader {
+    private val recordSize = Float.SIZE_BYTES * vectorDimensions + Int.SIZE_BYTES
+    private val totalVectorsCount = (channel.size() / recordSize).toInt()
+    private val buffer = ByteBuffer.allocate(recordSize)
+
+    init {
+        buffer.order(ByteOrder.LITTLE_ENDIAN)
+    }
+
+    override fun read(idx: Int): FloatArray {
+        val vector = FloatArray(vectorDimensions)
+        read(idx, vector)
+        return vector
+    }
+
+    override fun read(idx: Int, vector: FloatArray) {
+        while (buffer.hasRemaining()) {
+            channel.read(buffer, recordSize.toLong() * idx + buffer.position())
+        }
+        buffer.rewind()
+
+        check(buffer.getInt() == vectorDimensions) { "Vector dimensions mismatch" }
+
+        for (i in vector.indices) {
+            vector[i] = buffer.getFloat()
+        }
+        buffer.clear()
+    }
+
+    override fun readAll(): Array<FloatArray> {
+        val vectorCount = min(maxVectorCount, totalVectorsCount)
+        return Array(vectorCount) { i -> read(i) }
+    }
+
+    override fun close() {
+        channel.close()
+    }
+}
+
+class BvecsFileReader(
+    private val channel: FileChannel,
+    private val vectorDimensions: Int,
+    private val maxVectorCount: Int = Int.MAX_VALUE
+) : VectorFileReader {
+    private val recordSize = vectorDimensions + Int.SIZE_BYTES
+    private val totalVectorsCount = (channel.size() / recordSize).toInt()
+    private val buffer = ByteBuffer.allocate(recordSize)
+
+    init {
+        buffer.order(ByteOrder.LITTLE_ENDIAN)
+    }
+
+    override fun read(idx: Int): FloatArray {
+        val vector = FloatArray(vectorDimensions)
+        read(idx, vector)
+        return vector
+    }
+
+    override fun read(idx: Int, vector: FloatArray) {
+        while (buffer.hasRemaining()) {
+            channel.read(buffer, recordSize.toLong() * idx + buffer.position())
+        }
+        buffer.rewind()
+
+        check(buffer.getInt() == vectorDimensions) { "Vector dimensions mismatch" }
+
+        for (i in vector.indices) {
+            vector[i] = buffer.get().toFloat()
+        }
+        buffer.clear()
+    }
+
+    override fun readAll(): Array<FloatArray> {
+        val vectorCount = min(maxVectorCount, totalVectorsCount)
+        return Array(vectorCount) { i -> read(i) }
+    }
+
+    override fun close() {
+        channel.close()
+    }
+}
+
+fun readVectors(filePath: Path, vectorDimensions: Int, maxVectorCount: Int = Int.MAX_VALUE): Array<FloatArray> =
+    VectorFileReader.openFileReader(filePath, vectorDimensions, maxVectorCount).use { fileReader ->
+        fileReader.readAll()
+    }
