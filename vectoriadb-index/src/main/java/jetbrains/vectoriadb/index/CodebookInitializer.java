34ee1e7f1b2726cc62e784143c040642cc470021
Kirill Vasilenko <kirill.vasilenko@jetbrains.com>
2024-01-18 15:50:50 +0100

    convert CodebookInitializer from kotlin to java

---
+++ vectoriadb-index/src/main/java/jetbrains/vectoriadb/index/CodebookInitializer.java
@@ -0,0 +1,86 @@
+package jetbrains.vectoriadb.index;
+
+import java.util.Arrays;
+
+public class CodebookInitializer {
+    public static final int CODE_BASE_SIZE = 256;
+
+    private final int codebookCount;
+    private final int vectorDimensions;
+    private final int codeBaseSize;
+    private final int[] codebookDimensions;
+    private final int[] codebookDimensionOffset;
+    private final float[][][] codebooks;
+    private final int maxCodebookDimensions;
+
+    @SuppressWarnings("unused")
+    public int getCodebookCount() {
+        return codebookCount;
+    }
+
+    public int getCodeBaseSize() {
+        return codeBaseSize;
+    }
+
+    public int getVectorDimensions() {
+        return vectorDimensions;
+    }
+
+    public int[] getCodebookDimensions() {
+        return codebookDimensions;
+    }
+
+    public int[] getCodebookDimensionOffset() {
+        return codebookDimensionOffset;
+    }
+
+    public float[][][] getCodebooks() {
+        return codebooks;
+    }
+
+    public int getMaxCodebookDimensions() {
+        return maxCodebookDimensions;
+    }
+
+    public CodebookInitializer(int codebookCount, int vectorCount, int vectorDimensions) {
+        if (codebookCount < 0) {
+            throw new IllegalArgumentException("codebookCount < 0");
+        }
+        if (codebookCount > vectorDimensions) {
+            throw new IllegalArgumentException("codebookCount > vectorDimensions");
+        }
+
+        this.codebookCount = codebookCount;
+        this.vectorDimensions = vectorDimensions;
+        this.codeBaseSize = getCodeBaseSize(vectorCount);
+
+        int minCodebookDimensions = vectorDimensions / codebookCount;
+        int numCodebooksThatHaveOneExtraDimension = vectorDimensions % codebookCount;
+
+        this.codebookDimensions = new int[codebookCount];
+        this.codebookDimensionOffset = new int[codebookCount];
+
+        for (int codebookIdx = 0; codebookIdx < codebookCount; codebookIdx++) {
+            if (codebookIdx < numCodebooksThatHaveOneExtraDimension) {
+                this.codebookDimensions[codebookIdx] = minCodebookDimensions + 1;
+            } else {
+                this.codebookDimensions[codebookIdx] = minCodebookDimensions;
+            }
+            if (codebookIdx > 0) {
+                this.codebookDimensionOffset[codebookIdx] = codebookDimensionOffset[codebookIdx - 1] + codebookDimensions[codebookIdx - 1];
+            }
+        }
+        this.maxCodebookDimensions = codebookDimensions[0];
+        this.codebooks = new float[codebookCount][codeBaseSize][maxCodebookDimensions];
+        assert Arrays.stream(codebookDimensions).sum() == vectorDimensions;
+    }
+
+    public static int getCodebookCount(int vectorDimensions, int compressionRatio) {
+        int vectorSizeBytes = vectorDimensions * Float.BYTES;
+        return vectorSizeBytes / compressionRatio;
+    }
+
+    public static int getCodeBaseSize(int vectorCount) {
+        return Math.min(CODE_BASE_SIZE, vectorCount);
+    }
+}
