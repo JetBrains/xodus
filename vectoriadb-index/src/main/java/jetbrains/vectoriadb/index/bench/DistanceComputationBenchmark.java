35be5f8dc4334d6986f71ea908555a14d44c4a56
Kirill Vasilenko <kirill.vasilenko@jetbrains.com>
2024-01-29 14:51:24 +0100

    update DistanceComputationBenchmark results

--- vectoriadb-index/src/main/java/jetbrains/vectoriadb/index/bench/DistanceComputationBenchmark.java
+++ vectoriadb-index/src/main/java/jetbrains/vectoriadb/index/bench/DistanceComputationBenchmark.java
@@ -13,55 +13,63 @@ import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
-// Results measured:
-
-// 2024-01-12
-// Apple M1 Max, MaсOs 14.2.1 (23C71)
+// 2024-01-24
+// Ubuntu 22.04.3 LTS, AMD Ryzen 9 7950X3D 16-Core Processor
 // JMH version: 1.36
-// VM version: JDK 21.0.1, OpenJDK 64-Bit Server VM, 21.0.1+12-LTS
-// VM options: -Djava.awt.headless=true --enable-preview --add-modules=jdk.incubator.vector --enable-preview --add-modules=jdk.incubator.vector -Dfile.encoding=UTF-8 -Duser.country=NL -Duser.language=en -Duser.variant
-
+// VM version: JDK 21.0.2, Java HotSpot(TM) 64-Bit Server VM, 21.0.2+13-LTS-58
+// VM invoker: /home/kirill/.sdkman/candidates/java/21.0.2-oracle/bin/java
+// VM options: -Djava.awt.headless=true -XX:MaxDirectMemorySize=82g -XX:+HeapDumpOnOutOfMemoryError --add-modules=jdk.incubator.vector --enable-preview -Xmx16g -Dfile.encoding=UTF-8 -Duser.country -Duser.language=en -Duser.variant
 // Warmup: 5 iterations, 10 s each
 // Measurement: 5 iterations, 10 s each
-// Threads: 1 thread, will synchronize iterations
 
-//                                         (distanceFunction)  Mode  Cnt    Score    Error  Units
-// computeDistance_heap_array_1                            l2  avgt    5   14,098  ± 0,814  ns/op
-// computeDistance_heap_array_1                        l2-new  avgt    5   13,716  ± 0,104  ns/op
-// computeDistance_heap_array_1                            ip  avgt    5   11,549  ± 0,594  ns/op
-// computeDistance_heap_array_1                        ip-new  avgt    5   11,632  ± 0,543  ns/op
+// Benchmark                               (distanceFunction)  Mode  Cnt     Score    Error  Units
+// computeDistance_heap_array_1                      l2-cur-j  avgt   25    13.656 ±  0.315  ns/op
+// computeDistance_heap_array_1                      l2-new-k  avgt   25    44.482 ±  4.681  ns/op
+// computeDistance_heap_array_1                      l2-new-j  avgt   25    20.147 ±  8.867  ns/op
+// computeDistance_heap_array_1                      ip-cur-j  avgt   25    18.969 ±  6.871  ns/op
+// computeDistance_heap_array_1                      ip-new-k  avgt   25    26.011 ±  9.648  ns/op
+// computeDistance_heap_array_1                      ip-new-j  avgt   25    21.429 ±  9.173  ns/op
 
-// computeDistance_heap_array_4                            l2  avgt    5   64,918 ±  0,165  ns/op
-// computeDistance_heap_array_4                        l2-new  avgt    5   56,137 ±  0,408  ns/op
-// computeDistance_heap_array_4                            ip  avgt    5   56,997 ±  0,206  ns/op
-// computeDistance_heap_array_4                        ip-new  avgt    5   44,678 ±  0,038  ns/op
+// computeDistance_heap_array_4                      l2-cur-j  avgt   25   136.489 ± 40.330  ns/op
+// computeDistance_heap_array_4                      l2-new-k  avgt   25   116.688 ± 50.086  ns/op
+// computeDistance_heap_array_4                      l2-new-j  avgt   25   137.288 ± 61.257  ns/op
+// computeDistance_heap_array_4                      ip-cur-j  avgt   25   109.668 ± 33.747  ns/op
+// computeDistance_heap_array_4                      ip-new-k  avgt   25   191.941 ± 29.656  ns/op
+// computeDistance_heap_array_4                      ip-new-j  avgt   25   145.178 ± 54.418  ns/op
 
-// computeDistance_heap_array_4_batch                      l2  avgt    5   36,331 ±  0,924  ns/op
-// computeDistance_heap_array_4_batch                  l2-new  avgt    5   35,607 ±  0,581  ns/op
-// computeDistance_heap_array_4_batch                      ip  avgt    5   36,085 ±  0,314  ns/op
-// computeDistance_heap_array_4_batch                  ip-new  avgt    5   37,037 ±  1,383  ns/op
+// computeDistance_heap_array_4_batch                l2-cur-j  avgt   25  2725.085 ± 26.417  ns/op
+// computeDistance_heap_array_4_batch                l2-new-k  avgt   25   113.120 ± 52.378  ns/op
+// computeDistance_heap_array_4_batch                l2-new-j  avgt   25   126.899 ± 52.204  ns/op
+// computeDistance_heap_array_4_batch                ip-cur-j  avgt   25  2707.223 ± 32.452  ns/op
+// computeDistance_heap_array_4_batch                ip-new-k  avgt   25   111.451 ± 51.754  ns/op
+// computeDistance_heap_array_4_batch                ip-new-j  avgt   25   136.713 ± 51.090  ns/op
 
-// computeDistance_native_segment_1                        l2  avgt    5   23,344 ±  0,688  ns/op
-// computeDistance_native_segment_1                    l2-new  avgt    5   16,823 ±  0,352  ns/op
-// computeDistance_native_segment_1                        ip  avgt    5   20,724 ±  0,333  ns/op
-// computeDistance_native_segment_1                    ip-new  avgt    5   16,642 ±  0,970  ns/op
+// computeDistance_native_segment_1                  l2-cur-j  avgt   25    32.674 ± 14.303  ns/op
+// computeDistance_native_segment_1                  l2-new-k  avgt   25    29.257 ± 10.033  ns/op
+// computeDistance_native_segment_1                  l2-new-j  avgt   25    45.969 ± 10.563  ns/op
+// computeDistance_native_segment_1                  ip-cur-j  avgt   25    33.057 ±  9.898  ns/op
+// computeDistance_native_segment_1                  ip-new-k  avgt   25    26.914 ± 10.964  ns/op
+// computeDistance_native_segment_1                  ip-new-j  avgt   25    30.235 ± 11.799  ns/op
 
-// computeDistance_native_segment_4                        l2  avgt    5   97,697 ±  4,863  ns/op
-// computeDistance_native_segment_4                    l2-new  avgt    5   80,522 ±  1,557  ns/op
-// computeDistance_native_segment_4                        ip  avgt    5   96,511 ±  0,914  ns/op
-// computeDistance_native_segment_4                    ip-new  avgt    5  268,011 ±  7,819  ns/op
+// computeDistance_native_segment_4                  l2-cur-j  avgt   25   140.775 ± 46.405  ns/op
+// computeDistance_native_segment_4                  l2-new-k  avgt   25   197.088 ± 45.773  ns/op
+// computeDistance_native_segment_4                  l2-new-j  avgt   25   249.489 ± 60.843  ns/op
+// computeDistance_native_segment_4                  ip-cur-j  avgt   25   151.710 ± 40.663  ns/op
+// computeDistance_native_segment_4                  ip-new-k  avgt   25   285.272 ± 54.375  ns/op
+// computeDistance_native_segment_4                  ip-new-j  avgt   25   140.465 ± 56.175  ns/op
 
-// computeDistance_native_segment_4_batch                  l2  avgt    5   63,883 ±  1,094  ns/op
-// computeDistance_native_segment_4_batch              l2-new  avgt    5   46,069 ±  3,191  ns/op
-// computeDistance_native_segment_4_batch                  ip  avgt    5   58,537 ±  0,655  ns/op
-// computeDistance_native_segment_4_batch              ip-new  avgt    5   50,224 ±  3,479  ns/op
-
+// computeDistance_native_segment_4_batch            l2-cur-j  avgt   25  2712.345 ± 28.513  ns/op
+// computeDistance_native_segment_4_batch            l2-new-k  avgt   25   156.565 ± 54.092  ns/op
+// computeDistance_native_segment_4_batch            l2-new-j  avgt   25    96 .043 ± 43.784  ns/op
+// computeDistance_native_segment_4_batch            ip-cur-j  avgt   25  2707.009 ± 12.506  ns/op
+// computeDistance_native_segment_4_batch            ip-new-k  avgt   25   118.555 ± 50.070  ns/op
+// computeDistance_native_segment_4_batch            ip-new-j  avgt   25   180.573 ± 32.091  ns/op
 
 @SuppressWarnings("unused")
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-//@Fork(value = 1, warmups = 1) // uncomment it to make things faster
+@Fork(value = 1, warmups = 1) // uncomment it to make things faster
 public class DistanceComputationBenchmark {
     private static final int vectorSize = 128;
 
