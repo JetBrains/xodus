8090c1104f69aefd5c4ba81038234238f5255724
Vladimir Zatsepin <vladimir.zatsepin@jetbrains.com>
2024-03-14 15:54:41 +0100

    YTC-1792: Refactor entity id stuff

--- entity-store/src/main/kotlin/jetbrains/exodus/entitystore/orientdb/OEntity.kt
+++ entity-store/src/main/kotlin/jetbrains/exodus/entitystore/orientdb/OEntity.kt
@@ -1,13 +1,10 @@
 package jetbrains.exodus.entitystore.orientdb
 
-import com.orientechnologies.orient.core.id.ORID
 import jetbrains.exodus.entitystore.Entity
 
 interface OEntity : Entity {
 
     override fun getId(): OEntityId
-
-    fun getOId(): ORID
 
     fun save()
 }
--- entity-store/src/main/kotlin/jetbrains/exodus/entitystore/orientdb/ORIDEntityId.kt
+++ entity-store/src/main/kotlin/jetbrains/exodus/entitystore/orientdb/ORIDEntityId.kt
@@ -1,12 +1,35 @@
 package jetbrains.exodus.entitystore.orientdb
 
 import com.orientechnologies.orient.core.id.ORID
+import com.orientechnologies.orient.core.metadata.schema.OClass
+import com.orientechnologies.orient.core.record.OVertex
 import jetbrains.exodus.entitystore.EntityId
 
-class ORIDEntityId(private val id: ORID) : OEntityId {
+class ORIDEntityId(private val id: ORID, private val schemaClass: OClass) : OEntityId {
+
+    companion object {
+
+        fun fromVertex(vertex: OVertex): ORIDEntityId {
+            val schema = vertex.schemaClass
+            require(schema != null) { "Vertex $vertex must have a schema class" }
+            return ORIDEntityId(vertex.identity, schema)
+        }
+    }
+
+    override fun asOId(): ORID {
+        return id
+    }
 
     override fun getTypeId(): Int {
-        return id.clusterId
+        var id = 0
+        for (c in schemaClass.name) {
+            id = 31 * id + c.code
+        }
+        return id
+    }
+
+    fun getTypeName(): String {
+        return schemaClass.name
     }
 
     override fun getLocalId(): Long {
@@ -20,7 +43,7 @@ class ORIDEntityId(private val id: ORID) : OEntityId {
         return id.compareTo(other.id)
     }
 
-    override fun asOId(): ORID {
-        return id
+    override fun toString(): String {
+        return "${schemaClass}:${id}"
     }
 }
