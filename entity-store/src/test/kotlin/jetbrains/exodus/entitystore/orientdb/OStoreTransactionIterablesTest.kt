5cd7016b804ae0c15b529bf0929a5e02d52f66b9
Vladimir Zatsepin <vladimir.zatsepin@jetbrains.com>
2024-04-03 15:21:51 +0200

    XD-955: Refactor OStoreTransactionIterablesTest.kt

--- entity-store/src/test/kotlin/jetbrains/exodus/entitystore/orientdb/OStoreTransactionIterablesTest.kt
+++ entity-store/src/test/kotlin/jetbrains/exodus/entitystore/orientdb/OStoreTransactionIterablesTest.kt
@@ -132,269 +132,6 @@ class OStoreTransactionIterablesTest {
     }
 
     @Test
-    fun `should find property sorted by value`() {
-        // Given
-        val test = givenTestCase()
-        val tx = givenOTransaction()
-
-        orientDB.withSession {
-            test.issue1.setProperty("order", "1")
-            test.issue2.setProperty("order", "2")
-            test.issue3.setProperty("order", "3")
-        }
-
-        // When
-        orientDB.withSession {
-            val issuesAscending = tx.sort(Issues.CLASS, "order", true)
-            val issuesDescending = tx.sort(Issues.CLASS, "order", false)
-
-            // Then
-            assertNamesExactly(issuesAscending, "issue1", "issue2", "issue3")
-            assertNamesExactly(issuesDescending, "issue3", "issue2", "issue1")
-        }
-    }
-
-    @Test
-    fun `should find property exists and sorted by value`() {
-        // Given
-        val test = givenTestCase()
-        val tx = givenOTransaction()
-
-        orientDB.withSession {
-            test.issue1.setProperty("order", "1")
-            test.issue3.setProperty("order", "3")
-        }
-
-        // When
-        orientDB.withSession {
-            val issues = tx.findWithPropSortedByValue(Issues.CLASS, "order")
-            val issuesAscending = tx.sort(Issues.CLASS, "order", issues, true)
-            val issuesDescending = tx.sort(Issues.CLASS, "order", issues, false)
-
-            // Then
-            assertNamesExactly(issuesAscending, "issue1", "issue3")
-            assertNamesExactly(issuesDescending, "issue3", "issue1")
-        }
-    }
-
-
-    @Test
-    fun `should find union`() {
-        // Given
-        val test = givenTestCase()
-        val tx = givenOTransaction()
-
-        // When
-        orientDB.withSession {
-            val equal1 = tx.find(Issues.CLASS, "name", test.issue1.name())
-            val equal2 = tx.find(Issues.CLASS, "name", test.issue2.name())
-            val issues = equal1.union(equal2)
-
-            // Then
-            assertNamesExactly(issues, "issue1", "issue2")
-        }
-    }
-
-    @Test
-    fun `should find intersect`() {
-        // Given
-        val test = givenTestCase()
-        val tx = givenOTransaction()
-        orientDB.withSession {
-            test.issue2.setProperty(Issues.Props.PRIORITY, "normal")
-        }
-
-        // When
-        orientDB.withSession {
-            val nameEqual = tx.find(Issues.CLASS, "name", test.issue2.name())
-            val priorityEqual = tx.find(Issues.CLASS, Issues.Props.PRIORITY, "normal")
-            val issues = nameEqual.intersect(priorityEqual)
-
-            // Then
-            assertNamesExactly(issues, "issue2")
-            assertThat(issues.first().getProperty("priority")).isEqualTo("normal")
-        }
-    }
-
-    @Test
-    fun `should find by links`() {
-        // Given
-        val tx = givenOTransaction()
-        val testCase = givenTestCase()
-
-        orientDB.addIssueToProject(testCase.issue1, testCase.project1)
-        orientDB.addIssueToProject(testCase.issue2, testCase.project1)
-        orientDB.addIssueToProject(testCase.issue3, testCase.project2)
-
-        // When
-        orientDB.withSession {
-            val issues = tx.findLinks(Issues.CLASS, testCase.project1, Issues.Links.IN_PROJECT)
-
-            // Then
-            assertNamesExactly(issues, "issue1", "issue2")
-        }
-    }
-
-    @Test
-    fun `should find with links`() {
-        // Given
-        val test = givenTestCase()
-        val tx = givenOTransaction()
-
-        orientDB.addIssueToBoard(test.issue1, test.board1)
-        orientDB.addIssueToBoard(test.issue2, test.board2)
-
-        // When
-        orientDB.withSession {
-            val issuesOnBoard = tx.findWithLinks(Issues.CLASS, Issues.Links.ON_BOARD)
-            val issuesInProject = tx.findWithLinks(Issues.CLASS, Issues.Links.IN_PROJECT)
-
-            // Then
-            assertNamesExactly(issuesOnBoard, "issue1", "issue2")
-            assertThat(issuesInProject).isEmpty()
-        }
-    }
-
-    @Test
-    fun `should find links with union`() {
-        // Given
-        val testCase = givenTestCase()
-        val tx = givenOTransaction()
-
-        orientDB.addIssueToProject(testCase.issue1, testCase.project1)
-        orientDB.addIssueToProject(testCase.issue2, testCase.project1)
-        orientDB.addIssueToProject(testCase.issue3, testCase.project2)
-
-        // When
-        orientDB.withSession {
-            // Find all issues that in project1 or project2
-            val issuesInProject1 = tx.findLinks(Issues.CLASS, testCase.project1, Issues.Links.IN_PROJECT)
-            val issuesInProject2 = tx.findLinks(Issues.CLASS, testCase.project2, Issues.Links.IN_PROJECT)
-            val issues = issuesInProject1.union(issuesInProject2)
-
-            // Then
-            assertNamesExactly(issues, "issue1", "issue2", "issue3")
-        }
-    }
-
-    @Test
-    fun `should find links with intersect`() {
-        // Given
-        val test = givenTestCase()
-        val tx = givenOTransaction()
-
-        orientDB.addIssueToBoard(test.issue1, test.board1)
-        orientDB.addIssueToBoard(test.issue2, test.board1)
-        orientDB.addIssueToBoard(test.issue2, test.board2)
-        orientDB.addIssueToBoard(test.issue3, test.board3)
-
-        // When
-        orientDB.withSession {
-            // Find all issues that are on board1 and board2 at the same time
-            val issuesOnBoard1 = tx.findLinks(Issues.CLASS, test.board1, Issues.Links.ON_BOARD)
-            val issuesOnBoard2 = tx.findLinks(Issues.CLASS, test.board2, Issues.Links.ON_BOARD)
-            val issues = issuesOnBoard1.intersect(issuesOnBoard2)
-
-            // Then
-            assertNamesExactly(issues, "issue2")
-        }
-    }
-
-    @Test
-    fun `should find different links with union`() {
-        // Given
-        val test = givenTestCase()
-        val tx = givenOTransaction()
-
-        orientDB.addIssueToProject(test.issue1, test.project1)
-        orientDB.addIssueToBoard(test.issue2, test.board2)
-        orientDB.addIssueToBoard(test.issue3, test.board3)
-
-        // When
-        orientDB.withSession {
-            // Find all issues that are either in project1 or board2
-            val issuesOnBoard1 = tx.findLinks(Issues.CLASS, test.project1, Issues.Links.IN_PROJECT)
-            val issuesOnBoard2 = tx.findLinks(Issues.CLASS, test.board2, Issues.Links.ON_BOARD)
-            val issues = issuesOnBoard1.union(issuesOnBoard2)
-
-            // Then
-            assertNamesExactly(issues, "issue1", "issue2")
-        }
-    }
-
-    @Test
-    fun `should find by links sorted`() {
-        // Given
-        val test = givenTestCase()
-        val tx = givenOTransaction()
-
-        orientDB.addIssueToProject(test.issue1, test.project3)
-        orientDB.addIssueToProject(test.issue2, test.project2)
-        orientDB.addIssueToProject(test.issue3, test.project1)
-
-        // When
-        orientDB.withSession {
-            val links = tx.getAll(Projects.CLASS)
-            val issues = tx.getAll(Issues.CLASS)
-
-            val issueAsc = tx.sortLinks(
-                Issues.CLASS, // entity class
-                tx.sort(Projects.CLASS, "name", links, false), // links sorted asc by name
-                false, // is multiple
-                Issues.Links.IN_PROJECT, // link name
-                issues // entities
-            )
-            val issuesDesc = tx.sortLinks(
-                Issues.CLASS, // entity class
-                tx.sort(Projects.CLASS, "name", links, false), // links sorted desc by name
-                false, // is multiple
-                Issues.Links.IN_PROJECT, // link name
-                issues // entities
-            )
-
-            // Then
-            // As sorted by project name
-            assertNamesExactly(issueAsc, "issue3", "issue2", "issue1")
-            assertNamesExactly(issuesDesc, "issue1", "issue2", "issue3")
-        }
-    }
-
-    @Test
-    fun `should find by links sorted distinct`() {
-        // Given
-        val test = givenTestCase()
-        val tx = givenOTransaction()
-        // Issues assigned to projects in reverse order
-        orientDB.addIssueToProject(test.issue1, test.project3)
-        orientDB.addIssueToProject(test.issue1, test.project2)
-
-        orientDB.addIssueToProject(test.issue2, test.project2)
-        orientDB.addIssueToProject(test.issue2, test.project1)
-
-        orientDB.addIssueToProject(test.issue3, test.project1)
-        orientDB.addIssueToProject(test.issue3, test.project2)
-
-
-        // When
-        orientDB.withSession {
-            val links = tx.getAll(Projects.CLASS)
-            val issues = tx.getAll(Issues.CLASS)
-
-            // Find all issues that are either in project1 or board2
-            val issuesAsc = tx.sortLinks(
-                Issues.CLASS, // entity class
-                tx.sort(Projects.CLASS, "name", links, false), // links sorted desc by name
-                false, // is multiple
-                Issues.Links.IN_PROJECT, // link name
-                issues // entities
-            ).distinct()
-
-            // Then
-            assertNamesExactly(issuesAsc, "issue3", "issue2", "issue1")
-        }
-    }
-
-    @Test
     fun `should find entity with blob`() {
         // Given
         val test = givenTestCase()
@@ -426,6 +163,90 @@ class OStoreTransactionIterablesTest {
     }
 
     @Test
+    fun `should sorted by property`() {
+        // Given
+        val test = givenTestCase()
+        val tx = givenOTransaction()
+
+        orientDB.withSession {
+            test.issue1.setProperty("order", "1")
+            test.issue2.setProperty("order", "2")
+            test.issue3.setProperty("order", "3")
+        }
+
+        // When
+        orientDB.withSession {
+            val issuesAscending = tx.sort(Issues.CLASS, "order", true)
+            val issuesDescending = tx.sort(Issues.CLASS, "order", false)
+
+            // Then
+            assertNamesExactly(issuesAscending, "issue1", "issue2", "issue3")
+            assertNamesExactly(issuesDescending, "issue3", "issue2", "issue1")
+        }
+    }
+
+    @Test
+    fun `should sort iterable by property`() {
+        // Given
+        val test = givenTestCase()
+        val tx = givenOTransaction()
+
+        orientDB.withSession {
+            test.issue1.setProperty("order", "1")
+            test.issue3.setProperty("order", "3")
+        }
+
+        // When
+        orientDB.withSession {
+            val issues = tx.findWithPropSortedByValue(Issues.CLASS, "order")
+            val issuesAscending = tx.sort(Issues.CLASS, "order", issues, true)
+            val issuesDescending = tx.sort(Issues.CLASS, "order", issues, false)
+
+            // Then
+            assertNamesExactly(issuesAscending, "issue1", "issue3")
+            assertNamesExactly(issuesDescending, "issue3", "issue1")
+        }
+    }
+
+    @Test
+    fun `should iterable union`() {
+        // Given
+        val test = givenTestCase()
+        val tx = givenOTransaction()
+
+        // When
+        orientDB.withSession {
+            val equal1 = tx.find(Issues.CLASS, "name", test.issue1.name())
+            val equal2 = tx.find(Issues.CLASS, "name", test.issue2.name())
+            val issues = equal1.union(equal2)
+
+            // Then
+            assertNamesExactly(issues, "issue1", "issue2")
+        }
+    }
+
+    @Test
+    fun `should iterable intersect`() {
+        // Given
+        val test = givenTestCase()
+        val tx = givenOTransaction()
+        orientDB.withSession {
+            test.issue2.setProperty(Issues.Props.PRIORITY, "normal")
+        }
+
+        // When
+        orientDB.withSession {
+            val nameEqual = tx.find(Issues.CLASS, "name", test.issue2.name())
+            val priorityEqual = tx.find(Issues.CLASS, Issues.Props.PRIORITY, "normal")
+            val issues = nameEqual.intersect(priorityEqual)
+
+            // Then
+            assertNamesExactly(issues, "issue2")
+            assertThat(issues.first().getProperty("priority")).isEqualTo("normal")
+        }
+    }
+
+    @Test
     fun `should iterable concat`() {
         // Given
         val test = givenTestCase()
@@ -446,7 +267,6 @@ class OStoreTransactionIterablesTest {
             assertThat(concat).hasSize(3)
         }
     }
-
 
     @Test
     fun `should iterable distinct`() {
@@ -492,6 +312,184 @@ class OStoreTransactionIterablesTest {
 
             // Then
             assertNamesExactly(issues, "issue2", "issue3")
+        }
+    }
+
+    @Test
+    fun `should find links`() {
+        // Given
+        val tx = givenOTransaction()
+        val testCase = givenTestCase()
+
+        orientDB.addIssueToProject(testCase.issue1, testCase.project1)
+        orientDB.addIssueToProject(testCase.issue2, testCase.project1)
+        orientDB.addIssueToProject(testCase.issue3, testCase.project2)
+
+        // When
+        orientDB.withSession {
+            val issues = tx.findLinks(Issues.CLASS, testCase.project1, Issues.Links.IN_PROJECT)
+
+            // Then
+            assertNamesExactly(issues, "issue1", "issue2")
+        }
+    }
+
+    @Test
+    fun `should find with links`() {
+        // Given
+        val test = givenTestCase()
+        val tx = givenOTransaction()
+
+        orientDB.addIssueToBoard(test.issue1, test.board1)
+        orientDB.addIssueToBoard(test.issue2, test.board2)
+
+        // When
+        orientDB.withSession {
+            val issuesOnBoard = tx.findWithLinks(Issues.CLASS, Issues.Links.ON_BOARD)
+            val issuesInProject = tx.findWithLinks(Issues.CLASS, Issues.Links.IN_PROJECT)
+
+            // Then
+            assertNamesExactly(issuesOnBoard, "issue1", "issue2")
+            assertThat(issuesInProject).isEmpty()
+        }
+    }
+
+    @Test
+    fun `should find links and iterable union`() {
+        // Given
+        val testCase = givenTestCase()
+        val tx = givenOTransaction()
+
+        orientDB.addIssueToProject(testCase.issue1, testCase.project1)
+        orientDB.addIssueToProject(testCase.issue2, testCase.project1)
+        orientDB.addIssueToProject(testCase.issue3, testCase.project2)
+
+        // When
+        orientDB.withSession {
+            // Find all issues that in project1 or project2
+            val issuesInProject1 = tx.findLinks(Issues.CLASS, testCase.project1, Issues.Links.IN_PROJECT)
+            val issuesInProject2 = tx.findLinks(Issues.CLASS, testCase.project2, Issues.Links.IN_PROJECT)
+            val issues = issuesInProject1.union(issuesInProject2)
+
+            // Then
+            assertNamesExactly(issues, "issue1", "issue2", "issue3")
+        }
+    }
+
+    @Test
+    fun `should find links and iterable intersect`() {
+        // Given
+        val test = givenTestCase()
+        val tx = givenOTransaction()
+
+        orientDB.addIssueToBoard(test.issue1, test.board1)
+        orientDB.addIssueToBoard(test.issue2, test.board1)
+        orientDB.addIssueToBoard(test.issue2, test.board2)
+        orientDB.addIssueToBoard(test.issue3, test.board3)
+
+        // When
+        orientDB.withSession {
+            // Find all issues that are on board1 and board2 at the same time
+            val issuesOnBoard1 = tx.findLinks(Issues.CLASS, test.board1, Issues.Links.ON_BOARD)
+            val issuesOnBoard2 = tx.findLinks(Issues.CLASS, test.board2, Issues.Links.ON_BOARD)
+            val issues = issuesOnBoard1.intersect(issuesOnBoard2)
+
+            // Then
+            assertNamesExactly(issues, "issue2")
+        }
+    }
+
+    @Test
+    fun `should find different links and iterable union`() {
+        // Given
+        val test = givenTestCase()
+        val tx = givenOTransaction()
+
+        orientDB.addIssueToProject(test.issue1, test.project1)
+        orientDB.addIssueToBoard(test.issue2, test.board2)
+        orientDB.addIssueToBoard(test.issue3, test.board3)
+
+        // When
+        orientDB.withSession {
+            // Find all issues that are either in project1 or board2
+            val issuesOnBoard1 = tx.findLinks(Issues.CLASS, test.project1, Issues.Links.IN_PROJECT)
+            val issuesOnBoard2 = tx.findLinks(Issues.CLASS, test.board2, Issues.Links.ON_BOARD)
+            val issues = issuesOnBoard1.union(issuesOnBoard2)
+
+            // Then
+            assertNamesExactly(issues, "issue1", "issue2")
+        }
+    }
+
+    @Test
+    fun `should sort links by property`() {
+        // Given
+        val test = givenTestCase()
+        val tx = givenOTransaction()
+
+        orientDB.addIssueToProject(test.issue1, test.project3)
+        orientDB.addIssueToProject(test.issue2, test.project2)
+        orientDB.addIssueToProject(test.issue3, test.project1)
+
+        // When
+        orientDB.withSession {
+            val links = tx.getAll(Projects.CLASS)
+            val issues = tx.getAll(Issues.CLASS)
+
+            val issueAsc = tx.sortLinks(
+                Issues.CLASS, // entity class
+                tx.sort(Projects.CLASS, "name", links, false), // links sorted asc by name
+                false, // is multiple
+                Issues.Links.IN_PROJECT, // link name
+                issues // entities
+            )
+            val issuesDesc = tx.sortLinks(
+                Issues.CLASS, // entity class
+                tx.sort(Projects.CLASS, "name", links, false), // links sorted desc by name
+                false, // is multiple
+                Issues.Links.IN_PROJECT, // link name
+                issues // entities
+            )
+
+            // Then
+            // As sorted by project name
+            assertNamesExactly(issueAsc, "issue3", "issue2", "issue1")
+            assertNamesExactly(issuesDesc, "issue1", "issue2", "issue3")
+        }
+    }
+
+    @Test
+    fun `should sort links by property distinct`() {
+        // Given
+        val test = givenTestCase()
+        val tx = givenOTransaction()
+        // Issues assigned to projects in reverse order
+        orientDB.addIssueToProject(test.issue1, test.project3)
+        orientDB.addIssueToProject(test.issue1, test.project2)
+
+        orientDB.addIssueToProject(test.issue2, test.project2)
+        orientDB.addIssueToProject(test.issue2, test.project1)
+
+        orientDB.addIssueToProject(test.issue3, test.project1)
+        orientDB.addIssueToProject(test.issue3, test.project2)
+
+
+        // When
+        orientDB.withSession {
+            val links = tx.getAll(Projects.CLASS)
+            val issues = tx.getAll(Issues.CLASS)
+
+            // Find all issues that are either in project1 or board2
+            val issuesAsc = tx.sortLinks(
+                Issues.CLASS, // entity class
+                tx.sort(Projects.CLASS, "name", links, false), // links sorted desc by name
+                false, // is multiple
+                Issues.Links.IN_PROJECT, // link name
+                issues // entities
+            ).distinct()
+
+            // Then
+            assertNamesExactly(issuesAsc, "issue3", "issue2", "issue1")
         }
     }
 
