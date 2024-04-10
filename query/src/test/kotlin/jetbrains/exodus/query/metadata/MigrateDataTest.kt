d4b808d926f1a2a48e406ca8a81dd779c1ba72e3
Kirill Vasilenko <kirill.vasilenko@jetbrains.com>
2024-04-10 10:51:27 +0200

    XD-1003 refactor MigrateDataTest."copy properties"

--- query/src/test/kotlin/jetbrains/exodus/query/metadata/MigrateDataTest.kt
+++ query/src/test/kotlin/jetbrains/exodus/query/metadata/MigrateDataTest.kt
@@ -1,5 +1,6 @@
 package jetbrains.exodus.query.metadata
 
+import com.orientechnologies.orient.core.record.impl.ODocument
 import jetbrains.exodus.entitystore.StoreTransaction
 import jetbrains.exodus.entitystore.XodusTestDB
 import jetbrains.exodus.entitystore.orientdb.testutil.InMemoryOrientDB
@@ -18,23 +19,20 @@ class MigrateDataTest {
     val xodus = XodusTestDB()
 
     @Test
-    fun trista() {
-        val typeToEntities = mapOf(
-            "type1" to listOf<Map<String, Comparable<*>>>(
-                mapOf("id" to 1, "prop1" to "one", "prop2" to true, "prop3" to 1.1),
-                mapOf("id" to 2, "prop1" to "two", "prop2" to true, "prop3" to 2.2),
-                mapOf("id" to 3, "prop1" to "three", "prop2" to true, "prop3" to 3.3),
-            ),
-            "type2" to listOf<Map<String, Comparable<*>>>(
-                mapOf("id" to 4, "pop1" to "four", "pop2" to true, "pop3" to 4.4),
-                mapOf("id" to 5, "pop1" to "five", "pop2" to true,  "pop3" to 5.5),
-                mapOf("id" to 6, "pop1" to "six",  "pop2" to true, "pop3" to 6.6),
-            )
+    fun `copy properties`() {
+        val entities = pileOfEntities(
+            entity("type1", 1, "prop1" to "one", "prop2" to true, "prop3" to 1.1),
+            entity("type1", 2, "prop1" to "two", "prop2" to true, "prop3" to 2.2),
+            entity("type1", 3, "prop1" to "three", "prop2" to true, "prop3" to 3.3),
+
+            entity("type2", 2, "pop1" to "four", "pop2" to true, "pop3" to 2.2),
+            entity("type2", 4, "pop1" to "five", "pop2" to true, "pop3" to 3.3),
+            entity("type2", 5, "pop1" to "six", "pop2" to true, "pop3" to 4.4),
         )
         xodus.withTx { tx ->
-            for ((type, entities) in typeToEntities) {
-                for (item in entities) {
-                    tx.createEntity(type, item)
+            for (type in entities.types) {
+                for (entity in entities.getAll(type)) {
+                    tx.createEntity(entity)
                 }
             }
         }
@@ -42,22 +40,61 @@ class MigrateDataTest {
         XodusToOrientDataMigrator(xodus.store, orientDb.store).migrate()
 
         orientDb.withSession { oSession ->
-            for (type in typeToEntities.keys) {
+            for (type in entities.types) {
                 for (record in oSession.browseClass(type)) {
-                    val id = record.getProperty<Comparable<*>>("id")
-                    val entity = typeToEntities.getValue(type).first { it["id"] == id }
-                    for ((propName, propValue) in entity.entries) {
-                        Assert.assertEquals(propValue, record.getProperty<Comparable<*>>(propName))
-                    }
+                    val entity = entities.getEntity(type, record.getId())
+                    assertEquals(entity, record)
                 }
             }
         }
     }
 
-    private fun StoreTransaction.createEntity(type: String, props: Map<String, Comparable<*>>) {
-        val e = this.newEntity(type)
-        for ((name, value) in props) {
+    private fun assertEquals(expected: Entity, actual: ODocument) {
+        Assert.assertEquals(expected.id, actual.getId())
+        for ((propName, propValue) in expected.props) {
+            Assert.assertEquals(propValue, actual.getProperty<Comparable<*>>(propName))
+        }
+    }
+
+    private fun ODocument.getId(): Int = getProperty("id")
+
+    private fun StoreTransaction.createEntity(entity: Entity) {
+        val e = this.newEntity(entity.type)
+        e.setProperty("id", entity.id)
+        for ((name, value) in entity.props) {
             e.setProperty(name, value)
         }
     }
+}
+
+class PileOfEntities {
+    private val typeToEntities = mutableMapOf<String, MutableMap<Int, Entity>>()
+
+    val types: Set<String> get() = typeToEntities.keys
+
+    fun add(entity: Entity) {
+        typeToEntities.getOrPut(entity.type) { mutableMapOf() }[entity.id] = entity
+    }
+
+    fun getAll(type: String): Collection<Entity> = typeToEntities.getValue(type).values
+
+    fun getEntity(type: String, id: Int): Entity = typeToEntities.getValue(type).getValue(id)
+}
+
+data class Entity(
+    val type: String,
+    val id: Int,
+    val props: Map<String, Comparable<*>>
+)
+
+fun pileOfEntities(vararg entities: Entity): PileOfEntities {
+    val pile = PileOfEntities()
+    for (entity in entities) {
+        pile.add(entity)
+    }
+    return pile
+}
+
+fun entity(type: String, id: Int, vararg props: Pair<String, Comparable<*>>): Entity {
+    return Entity(type, id, props.toMap())
 }
