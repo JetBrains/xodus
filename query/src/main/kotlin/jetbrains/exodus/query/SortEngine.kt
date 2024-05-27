99f8b1e8903f36ea1dcd5983c376245d9e3df874
Leonid Stryuk <leonid.stryuk@jetbrains.com>
2024-05-27 17:25:31 +0200

    Ignored invalid tests. Waiting for solution from orient

--- query/src/main/kotlin/jetbrains/exodus/query/SortEngine.kt
+++ query/src/main/kotlin/jetbrains/exodus/query/SortEngine.kt
@@ -41,7 +41,7 @@ open class SortEngine {
     fun sort(enumType: String, propName: String, entityType: String, linkName: String, source: Iterable<Entity>, asc: Boolean): Iterable<Entity> {
         if (source is EntityIterable) {
             val txn = queryEngine.persistentStore.andCheckCurrentTransaction
-            return txn.sort(entityType, "${OVertexEntity.edgeClassName(linkName)}.$propName", source.unwrap(), asc)
+            return txn.sort(entityType, "$linkName.$propName", source, asc)
         } else {
             return if (asc) source.sortedBy { it.getLink(linkName)?.getProperty(propName) } else source.sortedByDescending { it.getLink(linkName)?.getProperty(propName) }
         }
