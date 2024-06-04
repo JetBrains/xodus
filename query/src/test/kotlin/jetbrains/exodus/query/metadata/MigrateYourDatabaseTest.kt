83efb52a678f790752d43ee99235707e3c340cce
Kirill Vasilenko <kirill.vasilenko@jetbrains.com>
2024-06-04 17:35:28 +0200

    XD-1053 Refactor data migration test in MigrateYourDatabaseTest
    The test for data migration from Xodus to OrientDB in `MigrateYourDatabaseTest.kt` has been simplified and cleaned up. Specific parameters and functions have been moved to a separate `XodusToOrientDataMigratorLauncher`. This introduced abstraction improves code readability and makes it more manageable.

--- query/src/test/kotlin/jetbrains/exodus/query/metadata/MigrateYourDatabaseTest.kt
+++ query/src/test/kotlin/jetbrains/exodus/query/metadata/MigrateYourDatabaseTest.kt
@@ -16,88 +16,41 @@
 package jetbrains.exodus.query.metadata
 
 import com.orientechnologies.orient.core.db.ODatabaseType
-import com.orientechnologies.orient.core.db.OrientDB
-import com.orientechnologies.orient.core.db.OrientDBConfig
-import jetbrains.exodus.entitystore.PersistentEntityStores
-import jetbrains.exodus.entitystore.orientdb.ODatabaseProviderImpl
-import jetbrains.exodus.entitystore.orientdb.OPersistentEntityStore
-import jetbrains.exodus.entitystore.orientdb.OSchemaBuddyImpl
-import jetbrains.exodus.env.Environments
-import jetbrains.exodus.env.newEnvironmentConfig
-import mu.KotlinLogging
+import org.junit.Ignore
 import org.junit.Test
-import kotlin.test.assertTrue
-
-private val log = KotlinLogging.logger { }
 
 class MigrateYourDatabaseTest {
 
-    val yourStoreName = "teamsysstore"
-    val yourXodusDatabaseFolder: String = "/Users/Kirill.Vasilenko/Downloads/ytdemoeng-backup/youtrack"
+    val yourStoreName = "yourStoreName"
+    val yourXodusDatabaseFolder: String = "/path/to/a/database"
     val yourCipherKey: String? = null
     val yourCipherIV = 0L
 
+    /**
+     * It is not a test to test some functionality.
+     * It is rather a convenient entry point for a developer to try migrating
+     * a local database with minimal hassle.
+     * So feel free to provide your parameters, comment @Ignore and run.
+     */
     @Test
+    @Ignore
     fun `migrate data from Xodus to OrientDB`() {
-
-        // 1. Where we migrate the data to
-
-        // 1.1 Create ODatabaseProvider
-        // params you need to provide through configs
-        val username = "admin"
-        val password = "password"
-        val dbName = "testDB"
-        val url = "memory"
-        // create the database
-        val db = OrientDB(url, OrientDBConfig.defaultConfig())
-        db.execute("create database $dbName MEMORY users ( $username identified by '$password' role admin )")
-        // create a provider
-        val dbProvider = ODatabaseProviderImpl(db, dbName, username, password, ODatabaseType.MEMORY)
-
-        // 1.2 Create OModelMetadata
-        // it is important to disable autoInitialize for the schemaBuddy,
-        // dataMigrator does not like anything existing in the database before it migrated the data
-        val schemaBuddy = OSchemaBuddyImpl(dbProvider, autoInitialize = false)
-        val oModelMetadata = OModelMetaData(dbProvider, schemaBuddy)
-
-        // 1.3 Create OPersistentEntityStore
-        // it is important to pass the oModelMetadata to the entityStore as schemaBuddy.
-        // it (oModelMetadata) must handle all the schema-related logic.
-        val oEntityStore = OPersistentEntityStore(dbProvider, dbName, schemaBuddy = oModelMetadata)
-
-        // 1.4 Create TransientEntityStore
-        // val oTransientEntityStore = TransientEntityStoreImpl(oModelMetadata, oEntityStore)
-
-
-        // 2. Where we migrate the data from
-
-        // 2.1 Create PersistentEntityStoreImpl
-        val env = Environments.newInstance(yourXodusDatabaseFolder, newEnvironmentConfig {
-            isLogProceedDataRestoredAtAnyCost = true
-            if (yourCipherKey != null) {
-                setCipherKey(yourCipherKey)
-                cipherBasicIV = yourCipherIV
-            }
-        })
-        val xEntityStore = PersistentEntityStores.newInstance(env, yourStoreName)
-
-        try {
-            xEntityStore.computeInTransaction { tx ->
-                assertTrue(tx.entityTypes.size > 0)
-            }
-
-            // 3. Migrate the data
-            migrateDataFromXodusToOrientDb(xEntityStore, oEntityStore)
-            schemaBuddy.initialize()
-
-            // 4. Check data is the same
-            checkDataIsSame(xEntityStore, oEntityStore)
-        } catch (e: Exception) {
-            throw e
-        } finally {
-            // cleanup
-            xEntityStore.close()
-            db.close()
-        }
+        val launcher = XodusToOrientDataMigratorLauncher(
+            orient = MigrateToOrientConfig(
+                databaseType = ODatabaseType.MEMORY,
+                url = "memory",
+                dbName = "testDB",
+                username = "admin",
+                password = "password"
+            ),
+            xodus = MigrateFromXodusConfig(
+                databaseDirectory = yourXodusDatabaseFolder,
+                storeName = yourStoreName,
+                cipherKey = yourCipherKey,
+                cipherIV = yourCipherIV
+            ),
+            validateDataAfterMigration = true
+        )
+        launcher.migrate()
     }
 }
