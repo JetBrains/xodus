f0c606b4214c2d88479af6d4d21da68b5c55ac30
Vladimir Zatsepin <vladimir.zatsepin@jetbrains.com>
2024-07-17 22:54:35 +0200

    XD-1065: Optimize functions for select by record ids

--- entity-store/src/main/kotlin/jetbrains/exodus/entitystore/orientdb/query/OQueryFunctions.kt
+++ entity-store/src/main/kotlin/jetbrains/exodus/entitystore/orientdb/query/OQueryFunctions.kt
@@ -71,6 +71,15 @@ object OQueryFunctions {
 
     fun difference(left: OSelect, right: OSelect): OSelect {
         return when {
+            left is ORecordIdSelect && right is ORecordIdSelect -> {
+                ensureLimitIsNotUsed(left, right)
+                ensureSkipIsNotUsed(left, right)
+
+                val newOrder = left.order.merge(right.order)
+                val ids = left.recordIds - right.recordIds.toSet()
+                ORecordIdSelect(ids, newOrder)
+            }
+
             left is OClassSelect && right is OClassSelect -> {
                 ensureInvariants(left, right)
 
