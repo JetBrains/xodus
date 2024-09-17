29c4052fe569d9b8b49711c8a8702059d13715a8
Kirill Vasilenko <kirill.vasilenko@jetbrains.com>
2024-09-17 18:47:18 +0200

    XD-1086 format code

--- openAPI/src/main/java/jetbrains/exodus/entitystore/EntityIterable.java
+++ openAPI/src/main/java/jetbrains/exodus/entitystore/EntityIterable.java
@@ -307,12 +307,11 @@ public interface EntityIterable extends Iterable<Entity> {
     @NotNull
     EntityIterable asSortResult();
 
-
-  /**
-   * This method is a default method is used to get the original {@code EntityIterable} when wrapped up.
-   *
-   * @return the unwrapped {@code EntityIterable}. By default, it returns the current instance.
-   */
+    /**
+     * This method is a default method is used to get the original {@code EntityIterable} when wrapped up.
+     *
+     * @return the unwrapped {@code EntityIterable}. By default, it returns the current instance.
+     */
     @NotNull
     EntityIterable unwrap();
 
