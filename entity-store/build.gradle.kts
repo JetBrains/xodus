22860d434337df289a67c9e3f809d8bb43a63f29
Lev Sivashov <lev.sivashov@jetbrains.com>
2025-02-26 12:26:25 +0100

    youtrackdb-core:1.0.0-20250226.111557-19

--- entity-store/build.gradle.kts
+++ entity-store/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api(project(":xodus-openAPI"))
-    api("io.youtrackdb:youtrackdb-core:1.0.0-20250218.130249-17")
+    api("io.youtrackdb:youtrackdb-core:1.0.0-20250226.111557-19")
 
     implementation(project(":xodus-utils"))
     implementation(project(":xodus-environment"))
