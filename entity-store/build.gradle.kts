400399711d537f8820ab70dbf5044751bc39cbc4
Lev Sivashov <lev.sivashov@jetbrains.com>
2025-04-25 08:03:43 +0200

    youtrackdb version upgrade

--- entity-store/build.gradle.kts
+++ entity-store/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api(project(":xodus-openAPI"))
-    api("io.youtrackdb:youtrackdb-core:1.0.0-20250423.081223-29")
+    api("io.youtrackdb:youtrackdb-core:1.0.0-20250425.055051-30")
 
     implementation(project(":xodus-utils"))
     implementation(project(":xodus-environment"))
